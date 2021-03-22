d0d6656b73e3fd2147d1649c47683a79bb8c1692
Craig Craig <craig@craig.se>
2021-03-22 15:50:15 -0500

    AssemblySpy class initial commit

---
+++ AstroClient/Cheetos/AssemblySpy.cs
@@ -0,0 +1,30 @@
+﻿using DayClientML2.Utility.Extensions;
+
+namespace AstroClient.Cheetos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AssemblySpy : Overridables
+    {
+        public List<Assembly> assembllies { get; private set; } = new List<Assembly>();
+
+        public AssemblySpy() : base()
+        {
+        }
+
+        public override void OnUpdate()
+        {
+            assembllies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+        }
+
+        public static List<Assembly> GetAssemblies()
+        {
+            return (Instance as AssemblySpy).assembllies;
+        }
+    }
+}
