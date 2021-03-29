e102f3dd919a9e1454271a688239e9e5be2e7f4c
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-03-29 15:45:48 +0200

    Reverting temporarily to Normal .Find as InactiveFind takes performance.

--- AstroClient/WorldAddons/Worlds/BClubWorld.cs
+++ AstroClient/WorldAddons/Worlds/BClubWorld.cs
@@ -62,12 +62,12 @@ namespace AstroClient
                 ModConsole.Log("Recognized " + WorldUtils.GetWorldName() + " World!");
 
                 ModConsole.Log("Searching for Private Rooms Exteriors...");
-                room1 = GameObjectFinder.InactiveFind("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 1");
-                room2 = GameObjectFinder.InactiveFind("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 2");
-                room3 = GameObjectFinder.InactiveFind("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 3");
-                room4 = GameObjectFinder.InactiveFind("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 4");
-                room5 = GameObjectFinder.InactiveFind("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 5");
-                room6 = GameObjectFinder.InactiveFind("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 6");
+                room1 = GameObjectFinder.Find("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 1");
+                room2 = GameObjectFinder.Find("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 2");
+                room3 = GameObjectFinder.Find("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 3");
+                room4 = GameObjectFinder.Find("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 4");
+                room5 = GameObjectFinder.Find("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 5");
+                room6 = GameObjectFinder.Find("nLobby/Private Rooms Exterior/Room Entrances/Private Room Entrance 6");
 
                 if (room1 != null && room2 != null && room3 != null && room4 != null && room5 != null && room6 != null)
                 {
@@ -79,11 +79,11 @@ namespace AstroClient
                     return;
                 }
 
-                room_1_BedroomPreview = GameObjectFinder.InactiveFind("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet/BlueButtonSquare - Bedroom Preview");
-                room_1_ToggleLocking = GameObjectFinder.InactiveFind("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet/BlueButtonWide - Toggle Looking");
-                room_1_ToggleLock = GameObjectFinder.InactiveFind("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet/BlueButtonWide - Toggle Lock");
-                room_1_ToggleIncognito = GameObjectFinder.InactiveFind("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet/BlueButtonWide - Toggle Incognito");
-                room_1_DND = GameObjectFinder.InactiveFind("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet Intercom/BlueButtonWide - Doorbell In DND");
+                room_1_BedroomPreview = GameObjectFinder.Find("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet/BlueButtonSquare - Bedroom Preview");
+                room_1_ToggleLocking = GameObjectFinder.Find("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet/BlueButtonWide - Toggle Looking");
+                room_1_ToggleLock = GameObjectFinder.Find("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet/BlueButtonWide - Toggle Lock");
+                room_1_ToggleIncognito = GameObjectFinder.Find("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet/BlueButtonWide - Toggle Incognito");
+                room_1_DND = GameObjectFinder.Find("/Bedrooms/Bedroom 1/BedroomUdon/Door Tablet Intercom/BlueButtonWide - Doorbell In DND");
 
 
             }
