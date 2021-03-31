142f1db62dac35078d0b7ac37d0188e1c26e8e24
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-03-31 20:58:46 +0200

    Add a janky udon search method.

---
+++ AstroClient/Utils/UdonSearcher/UdonSearch.cs
@@ -0,0 +1,39 @@
+﻿using AstroClient.ConsoleUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VRC.Udon;
+using static AstroClient.variables.CustomLists;
+
+namespace AstroClient
+{
+    public static class UdonSearch
+    {
+        
+        public static CachedUdonEvent FindUdonEvent(string action , string subaction)
+        {
+            var gameobjects = Resources.FindObjectsOfTypeAll<UdonBehaviour>();
+
+            var behaviour = gameobjects.Where(x => x.gameObject.name == action).DefaultIfEmpty(null).First();
+            if(behaviour != null)
+            {
+                ModConsole.DebugLog($"Found Behaviour {behaviour.gameObject.name}, Searching for Action.");
+                foreach(var actionkeys in behaviour._eventTable)
+                {
+                    if(actionkeys.key == subaction)
+                    {
+                        ModConsole.DebugLog($"Found subaction {actionkeys.key} bound in {behaviour.gameObject.name}");
+                        return new CachedUdonEvent(behaviour, actionkeys.key);
+                    }
+                }
+
+            }
+
+            return null;
+        }
+
+    }
+}
