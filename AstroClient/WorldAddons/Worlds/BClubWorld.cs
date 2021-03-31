2da30885682ffd0e840bf08922d4d7965ec69616
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-03-31 23:19:06 +0200

    Add all doors support <3

--- AstroClient/WorldAddons/Worlds/BClubWorld.cs
+++ AstroClient/WorldAddons/Worlds/BClubWorld.cs
@@ -27,25 +27,15 @@ namespace AstroClient
 
                 ModConsole.Log("Searching for Private Rooms Exteriors...");
 
-                var buttonGroup = CreateButtonGroup(1, new Vector3(-68.75317f, 15.9341f, -0.3320923f), new Quaternion(0.5209573f, 0.4646181f, -0.5404834f, -0.469693f));
-                //foreach (var action in UnityEngine.Object.FindObjectsOfType<UdonBehaviour>())
-                //{
-                //    if (action.name.ToLower() == "photozonemaster")
-                //    {
-                //        foreach (var subaction in action._eventTable)
-                //        {
-                //            ModConsole.Log($"Action {action.name} Contains Subaction {subaction.key}");
-                //        }
-                //    }
+                CreateButtonGroup(1, new Vector3(-84.66739f, 15.92832f, -0.3096771f), new Quaternion(-0.5241396f, -0.4620785f, -0.5343743f, -0.4756105f));
+                CreateButtonGroup(2, new Vector3(-83.13092f, 15.94455f, -4.310131f),  new Quaternion(0.5096976f, 0.4497673f, -0.5503947f, -0.4847511f));
+                CreateButtonGroup(3, new Vector3(-76.66214f, 15.96891f, -0.2979507f), new Quaternion(0.5298369f, 0.4824114f, 0.5115112f, 0.4742453f));
 
-                //    if (action.name.ToLower() == "rooms info master")
-                //    {
-                //        foreach (var subaction in action._eventTable)
-                //        {
-                //            ModConsole.Log($"Action {action.name} Contains Subaction {subaction.key}");
-                //        }
-                //    }
-                //}
+                CreateButtonGroup(4, new Vector3(-75.11559f, 15.92268f, -4.314186f), new Quaternion(0.5099828f, 0.4617184f, -0.5431892f, -0.4813308f));
+                CreateButtonGroup(5, new Vector3(-68.67709f, 15.94213f, -0.3036766f), new Quaternion(0.5483611f, 0.4571108f, 0.5301241f, 0.4575135f));
+                CreateButtonGroup(6, new Vector3(-67.13814f, 15.9624f, -4.320908f), new Quaternion(0.5246134f, 0.4534206f, -0.5460359f, -0.4701442f));
+
+
             }
         }
 
@@ -62,17 +52,17 @@ namespace AstroClient
             var room_ToggleIncognito = GameObjectFinder.Find($"/Bedrooms/Bedroom {doorID}/BedroomUdon/Door Tablet/BlueButtonWide - Toggle Incognito");
             var room_DND = GameObjectFinder.Find($"/Bedrooms/Bedroom {doorID}/BedroomUdon/Door Tablet Intercom/BlueButtonWide - Doorbell In DND");
 
-            GameObject buttonGroup = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            GameObject buttonGroup = new GameObject(); // Removed primitive sphere as is not needed unless debug.
             buttonGroup.transform.SetParent(room.transform);
-            buttonGroup.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f); // Just so I can see where the parent is for now
+            //buttonGroup.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f); // Just so I can see where the parent is for now
             buttonGroup.transform.position = position;
             buttonGroup.transform.rotation = rotation;
             buttonGroup.transform.position += new Vector3(0, 0.02f, 0);
             buttonGroup.AddToWorldUtilsMenu();
             buttonGroup.RenameObject($"ButtonGroup {doorID}");
-            buttonGroup.enablecolliders();
-            buttonGroup.ForcePickupComponent();
-            buttonGroup.SetPickupable(true);
+            //buttonGroup.enablecolliders();
+            //buttonGroup.ForcePickupComponent();
+            //buttonGroup.SetPickupable(true);
 
             // add buttons
             if (room_BedroomPreview != null && room_ToggleLooking != null && room_ToggleLock != null && room_ToggleIncognito != null && room_DND != null)
@@ -151,10 +141,7 @@ namespace AstroClient
                         clone.transform.SetParent(buttonGroup.transform);
                         clone.transform.position = buttonGroup.transform.position;
                         clone.transform.localPosition += new Vector3(2f, 0f, 0f);
-
-                        Vector3 rot = clone.transform.rotation.eulerAngles;
-                        rot = new Vector3(rot.x, rot.y, rot.z + 180);
-                        clone.transform.rotation = Quaternion.Euler(rot);
+                        clone.transform.rotation = new Quaternion(0f, 272f, 90f, 0f);
 
                         clone.AddToWorldUtilsMenu();
                         clone.RenameObject($"Do Not Disturb {doorID}");
