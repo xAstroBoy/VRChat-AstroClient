25ce6b640aea121e727d1a91b69d3a6c43f236f7
Craig Craig <craig@craig.se>
2021-04-02 16:21:19 -0500

    Removed VRC+/-, report, usericon, etc

--- AstroClient/Cheetos/CheetosUI.cs
+++ AstroClient/Cheetos/CheetosUI.cs
@@ -15,6 +15,7 @@
     using UnityEngine;
     using DayClientML2.Utility.Extensions;
     using VRC;
+    using AstroClient.extensions;
 
     public class CheetosPrivateStuff : Overridables
     {
@@ -26,6 +27,44 @@
             string VRChatVersion = VRCApplicationSetup.field_Private_Static_VRCApplicationSetup_0.prop_String_2;
             ModConsole.CheetoLog($"VRChat Version: {VRChatVersion}");
 #endif
+
+            // Find and remove shitty UI shit
+            var crap1 = GameObjectFinder.Find("UserInterface/QuickMenu/ShortcutMenu/ReportWorldButton");
+            var crap2 = GameObjectFinder.Find("UserInterface/QuickMenu/ShortcutMenu/UserIconButton");
+            var crap3 = GameObjectFinder.Find("UserInterface/QuickMenu/ShortcutMenu/VRCPlusMiniBanner");
+            var crap4 = GameObjectFinder.Find("UserInterface/QuickMenu/ShortcutMenu/HeaderContainer/VRCPlusBanner");
+            var crap5 = GameObjectFinder.Find("UserInterface/QuickMenu/ShortcutMenu/UserIconCameraButton");
+            var crap6 = GameObjectFinder.Find("UserInterface/QuickMenu/ShortcutMenu/VRCPlusThankYou");
+
+            if (crap1 != null)
+            {
+                crap1.DestroyMeLocal();
+            }
+
+            if (crap2 != null)
+            {
+                crap2.DestroyMeLocal();
+            }
+
+            if (crap3 != null)
+            {
+                crap3.DestroyMeLocal();
+            }
+
+            if (crap4 != null)
+            {
+                crap4.DestroyMeLocal();
+            }
+
+            if (crap5 != null)
+            {
+                crap5.DestroyMeLocal();
+            }
+
+            if (crap6 != null)
+            {
+                crap6.DestroyMeLocal();
+            }
         }
 
         public override void OnWorldReveal()
