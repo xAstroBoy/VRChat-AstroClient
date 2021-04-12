66031767ea8895e4398e8f113228d965ede8c03d
Craig Craig <craig@craig.se>
2021-04-12 15:27:44 -0500

    Loader's Assembly Info

--- AstroLoader/Properties/AssemblyInfo.cs
+++ AstroLoader/Properties/AssemblyInfo.cs
@@ -1,36 +1,25 @@
-﻿using System.Reflection;
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("AstroLoader")]
+[assembly: AssemblyTitle(AstroLoader.BuildInfo.Name)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AstroLoader")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany(AstroLoader.BuildInfo.Company)]
+[assembly: AssemblyProduct(AstroLoader.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + AstroLoader.BuildInfo.Author)]
+[assembly: AssemblyTrademark(AstroLoader.BuildInfo.Company)]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2baffc37-7a0e-4589-baa0-8f4e2c318747")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(AstroLoader.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(AstroLoader.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(AstroLoader.AstroLoader), AstroLoader.BuildInfo.Name, AstroLoader.BuildInfo.Version, AstroLoader.BuildInfo.Author, AstroLoader.BuildInfo.DownloadLink)]
+[assembly: InternalsVisibleTo("UnityExplorer.ML.IL2CPP")]
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
