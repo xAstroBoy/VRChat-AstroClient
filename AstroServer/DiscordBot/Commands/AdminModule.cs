351eddf33f89c2721467a6ce1ffcc85208cb7713
Craig Craig <craig@craig.se>
2021-05-01 03:42:51 -0500

    Discord stuff

--- AstroServer/DiscordBot/Commands/AdminModule.cs
+++ AstroServer/DiscordBot/Commands/AdminModule.cs
@@ -2,6 +2,7 @@
 {
 	using AstroServer.DiscordBot.Attributes;
 	using Discord.Commands;
+	using System.Collections.Generic;
 	using System.ComponentModel.DataAnnotations;
 	using System.Linq;
 	using System.Text;
@@ -16,23 +17,23 @@
 		[Summary("GenKey command")]
 		public async Task GenKey([Required] ulong discordID)
 		{
-			var key = RandomOrg.GetRandomKey();
+			string key = RandomOrg.GetRandomKey();
 
 			if (key != string.Empty)
 			{
 				if (!KeyManager.IsValidKey(key))
 				{
-					await ReplyAsync(null, false, CustomEmbed.GetNewKeyEmbed(key, discordID, true));
+					_ = await ReplyAsync(null, false, CustomEmbed.GetNewKeyEmbed(key, discordID, true));
 					KeyManager.AddKey(key, discordID);
 				}
 				else
 				{
-					await ReplyAsync(null, false, CustomEmbed.GetNewKeyEmbed(key, discordID, false));
+					_ = await ReplyAsync(null, false, CustomEmbed.GetNewKeyEmbed(key, discordID, false));
 				}
 			}
 			else
 			{
-				await ReplyAsync("Failed to generate key, string.Empty was returned.");
+				_ = await ReplyAsync("Failed to generate key, string.Empty was returned.");
 			}
 		}
 
@@ -40,23 +41,23 @@
 		[Summary("KeyCount command")]
 		public async Task KeyCount()
 		{
-			await ReplyAsync(null, false, CustomEmbed.GetKeyCountEmbed());
+			_ = await ReplyAsync(null, false, CustomEmbed.GetKeyCountEmbed());
 		}
 
 		[Command("ListKeys")]
 		[Summary("ListKeys command")]
 		public async Task ListKeys()
 		{
-			await ReplyAsync(null, false, CustomEmbed.GetKeyCountEmbed());
+			_ = await ReplyAsync(null, false, CustomEmbed.GetKeyCountEmbed());
 
-			foreach (var kvp in KeyManager.GetAllDevKeyInfo())
+			foreach (KeyValuePair<string, ulong> kvp in KeyManager.GetAllDevKeyInfo())
 			{
-				await base.ReplyAsync(null, false, CustomEmbed.GetKeyEmbed(kvp.Key));
+				_ = await base.ReplyAsync(null, false, CustomEmbed.GetKeyEmbed(kvp.Key));
 			}
 
-			foreach (var kvp in KeyManager.GetAllKeyInfo())
+			foreach (KeyValuePair<string, ulong> kvp in KeyManager.GetAllKeyInfo())
 			{
-				await base.ReplyAsync(null, false, CustomEmbed.GetKeyEmbed(kvp.Key));
+				_ = await base.ReplyAsync(null, false, CustomEmbed.GetKeyEmbed(kvp.Key));
 			}
 		}
 
@@ -64,63 +65,63 @@
 		[Summary("Notify command")]
 		public async Task Notify([Required] string name, [Required] string msg)
 		{
-			var found = ClientServer.Clients.Where(c => c.Name.Contains(name));
+			IEnumerable<Client> found = ClientServer.Clients.Where(c => c.Name.Contains(name));
 
 			StringBuilder stringBuilder = new StringBuilder();
-			foreach (var client in found)
+			foreach (Client client in found)
 			{
 				client.Send($"notify-dev:{msg}");
 				stringBuilder.Append($"Notified: {client.Name}, {client.UserID} \r\n");
 			}
 
-			await ReplyAsync(stringBuilder.ToString());
+			_ = await ReplyAsync(stringBuilder.ToString());
 		}
 
 		[Command("SendAll")]
 		[Summary("SendAll command")]
 		public async Task SendAll([Required] string cmd)
 		{
-			var found = ClientServer.Clients;
+			List<Client> found = ClientServer.Clients;
 
 			StringBuilder stringBuilder = new StringBuilder();
-			foreach (var client in found)
+			foreach (Client client in found)
 			{
 				client.Send(cmd);
 				stringBuilder.Append($"Command ran on: {client.Name}, {client.UserID} \r\n");
 			}
 
-			await ReplyAsync(stringBuilder.ToString());
+			_ = await ReplyAsync(stringBuilder.ToString());
 		}
 
 		[Command("Send")]
 		[Summary("Send command")]
 		public async Task Send([Required] string name, [Required] string cmd)
 		{
-			var found = ClientServer.Clients.Where(c => c.Name.Contains(name));
+			IEnumerable<Client> found = ClientServer.Clients.Where(c => c.Name.Contains(name));
 
 			StringBuilder stringBuilder = new StringBuilder();
-			foreach (var client in found)
+			foreach (Client client in found)
 			{
 				client.Send(cmd);
 				stringBuilder.Append($"Command ran on: {client.Name}, {client.UserID} \r\n");
 			}
-			await ReplyAsync(stringBuilder.ToString());
+			_ = await ReplyAsync(stringBuilder.ToString());
 		}
 
 		[Command("Kick")]
 		[Summary("Kick command")]
 		public async Task Kick([Required] string name)
 		{
-			var found = ClientServer.Clients.Where(c => c.Name.Contains(name));
+			IEnumerable<Client> found = ClientServer.Clients.Where(c => c.Name.Contains(name));
 
 			StringBuilder stringBuilder = new StringBuilder();
-			foreach (var client in found)
+			foreach (Client client in found)
 			{
 				client.Send("exit:you have been kicked");
 				stringBuilder.Append($"Kicked: {client.Name}, {client.UserID} \r\n");
 			}
 
-			await ReplyAsync(stringBuilder.ToString());
+			_ = await ReplyAsync(stringBuilder.ToString());
 		}
 
 		[Command("List")]
@@ -131,14 +132,14 @@
 			{
 				//stringBuilder.Append($"Client Count: {Server.Clients.Count} \r\n");
 
-				foreach (var client in ClientServer.Clients)
+				foreach (Client client in ClientServer.Clients)
 				{
-					await ReplyAsync(null, false, CustomEmbed.GetClientEmbed(client));
+					_ = await ReplyAsync(null, false, CustomEmbed.GetClientEmbed(client));
 				}
 			}
 			else
 			{
-				await ReplyAsync("There are no clients currently connected");
+				_ = await ReplyAsync("There are no clients currently connected");
 			}
 		}
 	}
