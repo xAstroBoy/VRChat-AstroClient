0a211d59faa654e6bee28f05d65fcc8b2b731301
Craig Craig <craig@craig.se>
2021-05-06 16:57:03 -0500

    StreamerProtector ExploitCheck

--- AstroClient/Cheetos/StreamerProtector.cs
+++ AstroClient/Cheetos/StreamerProtector.cs
@@ -1,6 +1,27 @@
 ﻿namespace AstroClient.Cheetos
 {
+	using DayClientML2.Utility.Extensions;
+	using System.Collections.Generic;
+
 	public class StreamerProtector : GameEvents
 	{
+		public static bool IsExploitsAllowed => ExploitCheck();
+
+		public static List<string> StreamerIDs = new List<string>();
+
+		private static bool ExploitCheck()
+		{
+			var players = WorldUtils.GetAllPlayers0();
+			
+			foreach (var player in players)
+			{
+				if (StreamerIDs.Contains(player.UserID()))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
 	}
 }
