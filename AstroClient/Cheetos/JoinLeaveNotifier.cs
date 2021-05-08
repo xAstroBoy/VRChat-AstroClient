70c8862ae8b5460ecd22c5ca87581ab3ec0cc4c5
Craig Craig <craig@craig.se>
2021-05-07 21:56:27 -0500

    JoinLeave notifier timer to prevent spam

--- AstroClient/Cheetos/JoinLeaveNotifier.cs
+++ AstroClient/Cheetos/JoinLeaveNotifier.cs
@@ -2,13 +2,39 @@
 {
 	using AstroClient.Cheetos;
 	using DayClientML2.Utility.Extensions;
+	using System.Timers;
 	using VRC;
 
 	internal class JoinLeaveNotifier : GameEvents
 	{
+		private static System.Timers.Timer aTimer;
+
+		private static bool isReady = false;
+
+		public override void OnWorldReveal(string id, string Name, string AssetURL)
+		{
+			SetTimer();
+			isReady = false;
+		}
+
+		private static void SetTimer()
+		{
+			// Create a timer with a two second interval.
+			aTimer = new System.Timers.Timer(5000);
+			// Hook up the Elapsed event for the timer. 
+			aTimer.Elapsed += OnTimedEvent;
+			aTimer.AutoReset = false;
+			aTimer.Enabled = true;
+		}
+
+		private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+		{
+			isReady = true;
+		}
+
 		public override void OnPlayerJoined(Player player)
 		{
-			if (ConfigManager.General.JoinLeave)
+			if (ConfigManager.General.JoinLeave && isReady)
 			{
 				CheetosHelpers.SendHudNotification($"Join: {player.DisplayName()}");
 			}
@@ -16,7 +42,7 @@
 
 		public override void OnPlayerLeft(Player player)
 		{
-			if (ConfigManager.General.JoinLeave)
+			if (ConfigManager.General.JoinLeave && isReady)
 			{
 				CheetosHelpers.SendHudNotification($"Leave: {player.DisplayName()}");
 			}
