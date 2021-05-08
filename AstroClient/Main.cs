83ac24157ba610823bfa8ca49800ea631b8791eb
Craig Craig <craig@craig.se>
2021-05-08 01:22:24 -0500

    Formatting fix

--- AstroClient/Main.cs
+++ AstroClient/Main.cs
@@ -59,12 +59,12 @@
 			KeyManager.IsAuthed = true;
 			Bools.IsDeveloper = true;
 #else
-            KeyManager.ReadKey();
-            AstroNetworkClient.Initialize();
+			KeyManager.ReadKey();
+			AstroNetworkClient.Initialize();
 
-            while (!KeyManager.IsAuthed)
-            {
-            }
+			while (!KeyManager.IsAuthed)
+			{
+			}
 #endif
 
 			try
@@ -132,7 +132,7 @@
 		{
 			//if (Application.targetFrameRate != int.MaxValue)
 			//{
-				//Application.targetFrameRate = int.MaxValue;
+			//Application.targetFrameRate = int.MaxValue;
 			//}
 			Event_OnUpdate?.Invoke(this, new EventArgs());
 		}
@@ -168,7 +168,7 @@
 				QMTabMenu AstroClient = new QMTabMenu(1f, "AstroClient Menu", null, null, null, Environment.CurrentDirectory + @"\AstroClient\Resources\planet.png");
 				ToggleDebugInfo = new QMSingleToggleButton(AstroClient, 4, 2.5f, "Debug Console ON", new Action(() => { Bools.IsDebugMode = true; }), "Debug Console OFF", new Action(() => { Bools.IsDebugMode = false; }), "Shows Client Details in Melonloader's console", UnityEngine.Color.green, UnityEngine.Color.red, null, false, true);
 				ToggleHideAvatar = new QMSingleToggleButton(AstroClient, 4, 1.5f, "Hide Avatar ON", () => { AvatarHider.HideAvatar = true; }, "Hide Avatar OFF", new Action(() => { AvatarHider.HideAvatar = false; }), "Hides your avatar for yourself", UnityEngine.Color.green, UnityEngine.Color.red, null, false, true);
-				
+
 				if (File.Exists(Environment.CurrentDirectory + @"\RubyClient\RubyClient.dll"))
 				{
 					ModConsole.Log("RubyClient Detected Buttons Moved");
@@ -181,8 +181,8 @@
 					ToggleNoClip = new QMSingleToggleButton(AstroClient, 0, -0.5f, "NoClip", () => { Flight.NoClipEnabled = true; }, "NoClip", () => { Flight.NoClipEnabled = false; }, "Enable/Disable NoClip", UnityEngine.Color.green, UnityEngine.Color.red, null, Flight.NoClipEnabled, true);
 				}
 
-				CopyIDButton = new QMSingleButton(AstroClient, 5, -1, "Copy\nInstance ID", delegate () { Clipboard.SetText($"{RoomManager.field_Internal_Static_ApiWorld_0.id}:{RoomManager.field_Internal_Static_ApiWorldInstance_0.idWithTags}");},"Copy the ID of the current instance.", null, null, true);
-				JoinInstanceButton = new QMSingleButton(AstroClient,5, -0.5f,"Join\nInstance",delegate (){new PortalInternal().Method_Private_Void_String_String_PDM_0(Clipboard.GetText().Split(':')[0], Clipboard.GetText().Split(':')[1]);},"Join an instance via your clipboard.", null, null, true);
+				CopyIDButton = new QMSingleButton(AstroClient, 5, -1, "Copy\nInstance ID", delegate () { Clipboard.SetText($"{RoomManager.field_Internal_Static_ApiWorld_0.id}:{RoomManager.field_Internal_Static_ApiWorldInstance_0.idWithTags}"); }, "Copy the ID of the current instance.", null, null, true);
+				JoinInstanceButton = new QMSingleButton(AstroClient, 5, -0.5f, "Join\nInstance", delegate () { new PortalInternal().Method_Private_Void_String_String_PDM_0(Clipboard.GetText().Split(':')[0], Clipboard.GetText().Split(':')[1]); }, "Join an instance via your clipboard.", null, null, true);
 				VRam = new QMSingleButton(AstroClient, 5, 0, "Clear\nVRAM", delegate () { var currentAvatars = (from player in PlayerManager.prop_PlayerManager_0.prop_ArrayOf_Player_0 where player != null select player.prop_ApiAvatar_0 into apiAvatar where apiAvatar != null select apiAvatar.assetUrl).ToList(); var dict = new Dictionary<string, Il2CppSystem.Object>(); var abdm = AssetBundleDownloadManager.prop_AssetBundleDownloadManager_0; foreach (var key in abdm.field_Private_Dictionary_2_String_Object_0.Keys) { dict.Add(key, abdm.field_Private_Dictionary_2_String_Object_0[key]); } foreach (var key in dict.Keys.Where(key => !abdm.field_Private_Dictionary_2_String_Object_0[key].name.Contains("vrcw") && !currentAvatars.Contains(key))) { abdm.field_Private_Dictionary_2_String_AssetBundleDownload_0.Remove(key); abdm.field_Private_Dictionary_2_String_Object_0.Remove(key); } dict.Clear(); GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true); Il2CppSystem.GC.Collect(GC.MaxGeneration, Il2CppSystem.GCCollectionMode.Forced, true, true); Resources.UnloadUnusedAssets(); ; }, "Clear VRAM.", null, null, true);
 
 				//FlightMenu.InitButtons(AstroClient, 1, 0, true);
@@ -191,7 +191,7 @@
 				Movement.InitButtons(AstroClient, 1, 1, true);
 				GameObjectUtils.InitButtons(AstroClient, 1, 1.5f, true);
 				EmojiUtils.InitButton(AstroClient, 1, 2, true);
-				if(Bools.IsDeveloper)
+				if (Bools.IsDeveloper)
 				{
 					MapEditorMenu.InitButtons(AstroClient, 1, 2.5f, true);
 				}
@@ -219,8 +219,8 @@
 		public static QMSingleToggleButton ToggleFly;
 		public static QMSingleToggleButton ToggleNoClip;
 
-        public static QMSingleButton CopyIDButton;
+		public static QMSingleButton CopyIDButton;
 		public static QMSingleButton JoinInstanceButton;
-        public static QMSingleButton VRam;
+		public static QMSingleButton VRam;
 	}
 }
