ecfd7d7ad22e3172644a1952d3b8b2697accda6b
Craig Craig <craig@craig.se>
2021-05-10 00:56:56 -0500

    New tab

--- AstroClient/Main.cs
+++ AstroClient/Main.cs
@@ -186,6 +186,7 @@
 				VRam = new QMSingleButton(AstroClient, 5, 0, "Clear\nVRAM", delegate () { var currentAvatars = (from player in PlayerManager.prop_PlayerManager_0.prop_ArrayOf_Player_0 where player != null select player.prop_ApiAvatar_0 into apiAvatar where apiAvatar != null select apiAvatar.assetUrl).ToList(); var dict = new Dictionary<string, Il2CppSystem.Object>(); var abdm = AssetBundleDownloadManager.prop_AssetBundleDownloadManager_0; foreach (var key in abdm.field_Private_Dictionary_2_String_Object_0.Keys) { dict.Add(key, abdm.field_Private_Dictionary_2_String_Object_0[key]); } foreach (var key in dict.Keys.Where(key => !abdm.field_Private_Dictionary_2_String_Object_0[key].name.Contains("vrcw") && !currentAvatars.Contains(key))) { abdm.field_Private_Dictionary_2_String_AssetBundleDownload_0.Remove(key); abdm.field_Private_Dictionary_2_String_Object_0.Remove(key); } dict.Clear(); GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true); Il2CppSystem.GC.Collect(GC.MaxGeneration, Il2CppSystem.GCCollectionMode.Forced, true, true); Resources.UnloadUnusedAssets(); ; }, "Clear VRAM.", null, null, true);
 
 				FlightMenu.InitButtons(AstroClient, 1, 0, true);
+				ExploitsMenu.InitButtons(AstroClient, 1, 0, true);
 				WorldsCheats.InitButtons(AstroClient, 1, 0, true);
 				LightControl.InitButtons(AstroClient, 1, 0.5f, true);
 				Movement.InitButtons(AstroClient, 1, 1, true);
