0d16132aee4d65c5bcf1d96a6e542c71eb5c054e
Craig Craig <craig@craig.se>
2021-05-11 09:43:38 -0500

    Fix

--- AstroServer/InstanceManager.cs
+++ AstroServer/InstanceManager.cs
@@ -12,19 +12,19 @@
 			{
 				if (other.InstanceID.Equals(client.InstanceID))
 				{
-					if (client.IsDeveloper)
-					{
-						TagData tagData = new TagData() { UserID = client.UserID, Text = "AstroClient Developer" };
-						var json = JsonConvert.SerializeObject(tagData);
-						other.Send(new PacketData(PacketServerType.ADD_TAG, json));
-						other.Send(new PacketData(PacketServerType.NOTIFY, $"<color=cyan>AstroClient Developer</color> {client.Name} Is Here!"));
-					}
-					else
-					{
-						TagData tagData = new TagData() { UserID = client.UserID, Text = "AstroClient" };
-						var json = JsonConvert.SerializeObject(tagData);
-						other.Send(new PacketData(PacketServerType.ADD_TAG, json));
-					}
+					//if (client.IsDeveloper)
+					//{
+					//	TagData tagData = new TagData() { UserID = client.UserID, Text = "AstroClient Developer" };
+					//	var json = JsonConvert.SerializeObject(tagData);
+					//	other.Send(new PacketData(PacketServerType.ADD_TAG, json));
+					//	other.Send(new PacketData(PacketServerType.NOTIFY, $"<color=cyan>AstroClient Developer</color> {client.Name} Is Here!"));
+					//}
+					//else
+					//{
+					//	TagData tagData = new TagData() { UserID = client.UserID, Text = "AstroClient" };
+					//	var json = JsonConvert.SerializeObject(tagData);
+					//	other.Send(new PacketData(PacketServerType.ADD_TAG, json));
+					//}
 
 					if (other.IsDeveloper)
 					{
