397b24a7a33b089404cb0bbc699f5f60300affdb
Craig Craig <craig@craig.se>
2021-05-11 05:24:07 -0500

    Log avatar id to console for now

--- AstroServer/ClientServer.cs
+++ AstroServer/ClientServer.cs
@@ -140,6 +140,13 @@
 				InstanceManager.InstanceJoined(client);
 			}
 
+			if (packetData.NetworkEventID == PacketClientType.AVATAR_DATA)
+			{
+				AvatarData avatarData = BSonWriter.FromBson<AvatarData>(packetData.ByteData.ConvertToString());
+
+				Console.WriteLine($"AvatarData for {avatarData.ID} received.");
+			}
+
 			//if (first.Equals("key"))
 			//{
 			//	string key = second;
