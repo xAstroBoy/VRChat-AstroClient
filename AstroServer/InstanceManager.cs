91416ccfd7b6bca71a79502909105c95e5927cab
Craig Craig <craig@craig.se>
2021-05-11 06:06:46 -0500

    Tweaks

--- AstroServer/InstanceManager.cs
+++ AstroServer/InstanceManager.cs
@@ -25,6 +25,21 @@
 						TagData tagData = new TagData() { UserID = client.UserID, Text = "AstroClient" };
 						other.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData));
 					}
+
+					if (other.IsDeveloper)
+					{
+						TagData tagData = new TagData() { UserID = other.UserID, Text = "AstroClient Developer" };
+						client.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData));
+						if (!client.ClientID.Equals(other.ClientID))
+						{
+							client.Send(new PacketData(PacketServerType.NOTIFY, $"<color=cyan>AstroClient Developer</color> {client.Name} Is Here!"));
+						}
+					}
+					else
+					{
+						TagData tagData = new TagData() { UserID = other.UserID, Text = "AstroClient" };
+						client.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData));
+					}
 				}
 			}
 		}
