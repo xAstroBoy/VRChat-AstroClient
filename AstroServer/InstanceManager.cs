d4e5937bcdcb076a542458cbd600c74e25aea2e3
Craig Craig <craig@craig.se>
2021-05-11 04:18:38 -0500

    Tag fixes

--- AstroServer/InstanceManager.cs
+++ AstroServer/InstanceManager.cs
@@ -13,20 +13,14 @@
 				{
 					if (client.IsDeveloper)
 					{
-						TagData tagData1 = new TagData() { UserID = client.UserID, Text = "AstroClient Developer" };
-						other.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData1));
+						TagData tagData = new TagData() { UserID = client.UserID, Text = "AstroClient Developer" };
+						other.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData));
 					}
-
-					if (other.IsDeveloper)
+					else
 					{
-						TagData tagData1 = new TagData() { UserID = other.UserID, Text = "AstroClient Developer" };
-						client.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData1));
+						TagData tagData = new TagData() { UserID = client.UserID, Text = "AstroClient" };
+						other.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData));
 					}
-
-					TagData tagData2 = new TagData() { UserID = client.UserID, Text = "AstroClient" };
-					TagData tagData3 = new TagData() { UserID = other.UserID, Text = "AstroClient" };
-					other.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData2));
-					client.Send(new PacketData(PacketServerType.ADD_TAG, "", null, tagData3));
 				}
 			}
 		}
