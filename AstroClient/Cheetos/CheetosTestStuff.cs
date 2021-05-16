3de95d6e83d6889983b5be23dc7ee7e98eca4750
Craig Craig <craig@craig.se>
2021-05-16 05:11:58 -0500

    Always move infoBar down

--- AstroClient/Cheetos/CheetosTestStuff.cs
+++ AstroClient/Cheetos/CheetosTestStuff.cs
@@ -3,8 +3,6 @@
 	using AstroLibrary.Console;
 	using AstroClient.Finder;
 	using AstroClient.variables;
-	using System;
-	using System.IO;
 	using UnityEngine;
 
 	internal class CheetosTestStuff : GameEvents
@@ -21,21 +19,8 @@
 			var infobartext = GameObject.Find("UserInterface/QuickMenu/QuickMenu_NewElements/_InfoBar/EarlyAccessText").GetComponent<UnityEngine.UI.Text>();
 			infobartext.color = new Color(1, 0, 1, 1);
 
-			if (File.Exists(Environment.CurrentDirectory + @"\Mods\Notorious.dll"))
-			{
-				if (infoBar != null)
-				{
-					infoBar.transform.localPosition -= new Vector3(0, 110, 0);
-				}
-			}
-			else
-			{
-				if (infoBar != null)
-				{
-					infoBar.transform.localPosition += new Vector3(0, 80, 0);
-					infobartext.GetComponent<UnityEngine.UI.Text>().text = "AstroClient";
-				}
-			}
+			infoBar.transform.localPosition -= new Vector3(0, 110, 0);
+			infobartext.GetComponent<UnityEngine.UI.Text>().text = "AstroClient";
 
 			ModConsole.CheetoLog($"VRChat Version: {VRChatVersion}, {VRChatBuild}");
 		}
