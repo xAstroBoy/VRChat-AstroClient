857eac6986cd49916d1ea1b80a59c7616502662a
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-05-26 20:36:18 +0200

    Bonk da cheeto for the error lol

--- AstroClient/CheetosUI/CheetoMenu.cs
+++ AstroClient/CheetosUI/CheetoMenu.cs
@@ -1,5 +1,6 @@
 ﻿namespace AstroClient
 {
+	using AstroLibrary.Console;
 	#region Imports
 
 	using AstroLibrary.Finder;
@@ -24,35 +25,44 @@
 
 		public override void VRChat_OnUiManagerInit()
 		{
-			var camera = GameObjectFinder.Find("_Application/TrackingVolume/TrackingSteam(Clone)/SteamCamera/[CameraRig]/Neck/Camera (head)/Camera (eye)").GetComponent<Camera>();
-			standaloneInputModule = GameObjectFinder.Find("_Application/UiEventSystem").GetComponent<VRCStandaloneInputModule>();
-			cursorManager = GameObjectFinder.Find("_Application/CursorManager").GetComponent<VRCUiCursorManager>();
-			UI = new GameObject() { name = "CheetoUI" };
-			UI.name = "CheetoUI";
-			UI.layer = LayerMask.NameToLayer("UI");
+			try
+			{
+				var camera = GameObjectFinder.Find("_Application/TrackingVolume/TrackingSteam(Clone)/SteamCamera/[CameraRig]/Neck/Camera (head)/Camera (eye)").GetComponent<Camera>();
+				standaloneInputModule = GameObjectFinder.Find("_Application/UiEventSystem").GetComponent<VRCStandaloneInputModule>();
+				cursorManager = GameObjectFinder.Find("_Application/CursorManager").GetComponent<VRCUiCursorManager>();
+				UI = new GameObject() { name = "CheetoUI" };
+				UI.name = "CheetoUI";
+				UI.layer = LayerMask.NameToLayer("UI");
 
-			Menu = new GameObject() { name = "CheetoMenu" };
-			Menu.layer = LayerMask.NameToLayer("UiMenu");
-			Menu.AddComponent<RectTransform>();
-			Menu.transform.SetParent(UI.transform, false);
-			Menu.GetComponent<RectTransform>().sizeDelta = new Vector2(800, 600);
-			Menu.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.5f, 0.5f);
-			Menu.AddComponent<BoxCollider>();
-			Menu.GetComponent<BoxCollider>().enabled = false;
-			Menu.AddComponent<Canvas>();
-			Menu.GetComponent<Canvas>().renderMode = RenderMode.WorldSpace;
-			Menu.GetComponent<Canvas>().worldCamera = camera;
-			Menu.AddComponent<CanvasGroup>();
-			Menu.AddComponent<GraphicRaycaster>();
-			Menu.GetComponent<GraphicRaycaster>().enabled = true;
-			Menu.GetComponent<GraphicRaycaster>().m_BlockingMask = 0;
+				Menu = new GameObject() { name = "CheetoMenu" };
+				Menu.layer = LayerMask.NameToLayer("UiMenu");
+				Menu.AddComponent<RectTransform>();
+				Menu.transform.SetParent(UI.transform, false);
+				Menu.GetComponent<RectTransform>().sizeDelta = new Vector2(800, 600);
+				Menu.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.5f, 0.5f);
+				Menu.AddComponent<BoxCollider>();
+				Menu.GetComponent<BoxCollider>().enabled = false;
+				Menu.AddComponent<Canvas>();
+				Menu.GetComponent<Canvas>().renderMode = RenderMode.WorldSpace;
+				Menu.GetComponent<Canvas>().worldCamera = camera;
+				Menu.AddComponent<CanvasGroup>();
+				Menu.AddComponent<GraphicRaycaster>();
+				Menu.GetComponent<GraphicRaycaster>().enabled = true;
+				Menu.GetComponent<GraphicRaycaster>().m_BlockingMask = 0;
 
-			_ = new CheetoBackground(Menu.transform);
-			_ = new CheetoPage(Menu.transform);
+				_ = new CheetoBackground(Menu.transform);
+				_ = new CheetoPage(Menu.transform);
 
-			Menu.SetActive(false);
-			Object.DontDestroyOnLoad(UI);
+				Menu.SetActive(false);
+				Object.DontDestroyOnLoad(UI);
+			}
+			catch (System.Exception e)
+			{
+				ModConsole.DebugError("Exception Generating CheetoMenu!");
+				ModConsole.DebugErrorExc(e);
+			}
 		}
+			
 
 		public override void OnUpdate()
 		{
