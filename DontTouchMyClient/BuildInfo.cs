c3be7a950a377d981ca2199d568fe498fe32ded3
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-06-03 12:14:35 +0200

    Merge DontTouchMyClient into repo
    And Hide Lewdifier Duplicate spam check lol

---
+++ DontTouchMyClient/BuildInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace DontTouchMyClient
+{
+    public static class BuildInfo
+    {
+        public const string Name = "DontTouchMyClient"; // Name of the Plugin.  (MUST BE SET)
+        public const string Description = "Protects Your Game Session."; // Description for the Plugin.  (Set as null if none)
+        public const string Author = "xAstroBoy, Cheetos"; // Author of the Plugin.  (Set as null if none)
+        public const string Company = null; // Company that made the Plugin.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Plugin.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Plugin.  (Set as null if none)
+    }
+}
---
+++ DontTouchMyClient/Main.cs
@@ -0,0 +1,30 @@
+﻿using DontTouchMyClient.Patches;
+using Harmony;
+using MelonLoader;
+using System;
+using System.Reflection;
+namespace DontTouchMyClient
+{
+
+    public class DontTouchMyClient : MelonPlugin
+    {
+        public override void OnPreInitialization() // Runs before Game Initialization.
+        {
+            Patching.StartDefenses();
+        }
+        public override void OnGUI()
+        {
+            PreventFileSearches = true;
+            NeedsToBlockRegistry = true;
+            HideModsAndPlugins = true;
+        }
+
+        public static bool ProtectVRChatProcessByHidingIt = false;
+
+        public static bool NeedsToBlockRegistry = false;
+
+        public static bool PreventFileSearches = false;
+
+        public static bool HideModsAndPlugins = true;
+    }
+}
