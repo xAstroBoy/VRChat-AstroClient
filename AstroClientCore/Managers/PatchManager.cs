708d25c4bb2356255665e39280ff0a437d9d5843
Craig Craig <craig@craig.se>
2021-06-08 03:26:35 -0500

    Some patch stuff for core

---
+++ AstroClientCore/Managers/PatchManager.cs
@@ -0,0 +1,41 @@
+﻿namespace AstroClientCore.Managers
+{
+	using AstroLibrary.Console;
+	using Harmony;
+	using System.Collections.Generic;
+	using System.Reflection;
+
+	public static class PatchManager
+	{
+		private static HarmonyInstance harmony;
+
+		private static List<Patching.Patch> patches = new List<Patching.Patch>();
+
+		public static HarmonyMethod GetPatch<T>(string name)
+		{
+			return new HarmonyMethod(typeof(T).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic));
+		}
+
+		public static void Initialize()
+		{
+			if (harmony == null)
+			{
+				harmony = HarmonyInstance.Create("AstroClient.PatchManager");
+				ModConsole.Log("[PatchManager] Harmony Instance Created.");
+			}
+		}
+
+		public static void AddPatch(Patching.Patch patch)
+		{
+			patches.Add(patch);
+		}
+
+		public static void DoPatches()
+		{
+			foreach (var patch in patches)
+			{
+				patch.DoPatch();
+			}
+		}
+	}
+}
