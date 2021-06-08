493692abc901711e115fd3b86d782aa7c06445fc
Craig Craig <craig@craig.se>
2021-06-07 21:17:47 -0500

    Buttons to manually set speeds

--- AstroClient/Buttons/FlightMenu.cs
+++ AstroClient/Buttons/FlightMenu.cs
@@ -1,36 +1,74 @@
 ﻿namespace AstroClient.Startup.Buttons
 {
 	using AstroClient.Cheetos;
+	using AstroLibrary.Console;
 	using DayClientML2.Utility.Extensions;
 	using RubyButtonAPI;
 	using UnityEngine;
 
 	internal class FlightMenu : GameEvents
 	{
+		private static QMNestedButton mainButton;
+
+		private static QMSlider desktopSpeedSlider;
+
+		private static QMSlider vrSpeedSlider;
+
+		private static QMSingleButton setPCSpeedButton;
+
+		private static QMSingleButton setVRSpeedButton;
+
 		public static void InitButtons(QMTabMenu menu, float x, float y, bool btnHalf)
 		{
-			var sub = new QMNestedButton(menu, x, y, "Fly Menu", "Fly Options", null, null, null, null, btnHalf);
+			mainButton = new QMNestedButton(menu, x, y, "Fly Menu", "Fly Options", null, null, null, null, btnHalf);
 
-			QMSlider desktopSpeedSlider = new QMSlider(Utils.QuickMenu.transform.Find(sub.GetMenuName()), "pc Speed", 400, -620, delegate (float value) { Flight.SetDesktopFlySpeed(value); }, ConfigManager.Flight.VRFlySpeed, 20, 1, true);
+			desktopSpeedSlider = new QMSlider(Utils.QuickMenu.transform.Find(mainButton.GetMenuName()), "PC Speed", 400, -620, delegate (float value) { Flight.SetDesktopFlySpeed(value); }, ConfigManager.Flight.VRFlySpeed, 20, 1, true);
 			desktopSpeedSlider.Slider.transform.localScale = new Vector3(3.5f, 3.5f, 3.5f);
 
-			new QMSingleButton(sub, 3, 3, "Set\nPC\nSpeed", delegate ()
+			setPCSpeedButton = new QMSingleButton(mainButton, 1, 2, "Set\nPC\nSpeed", delegate ()
 			{
 				CheetosHelpers.PopupCall("Set PC Speed", "Done", "Enter Speed. . .", true, delegate (string text)
 				{
+					float value = 0f;
 
+					try
+					{
+						value = float.Parse(text);
+					}
+					catch
+					{
+						ModConsole.Error("Input value must be a float value!");
+					}
+					finally
+					{
+						desktopSpeedSlider.SetValue(value);
+						Flight.SetDesktopFlySpeed(value);
+					}
 				});
 			}, "Manually input a PC speed");
 
-
-			QMSlider vrSpeedSlider = new QMSlider(Utils.QuickMenu.transform.Find(sub.GetMenuName()), "VR Speed", 400, -820, delegate (float value) { Flight.SetVRFlySpeed(value); }, ConfigManager.Flight.VRFlySpeed, 20, 1, true);
+			vrSpeedSlider = new QMSlider(Utils.QuickMenu.transform.Find(mainButton.GetMenuName()), "VR Speed", 400, -820, delegate (float value) { Flight.SetVRFlySpeed(value); }, ConfigManager.Flight.VRFlySpeed, 20, 1, true);
 			vrSpeedSlider.Slider.transform.localScale = new Vector3(3.5f, 3.5f, 3.5f);
 
-			new QMSingleButton(sub, 3, 4, "Set\nVR\nSpeed", delegate ()
+			setVRSpeedButton = new QMSingleButton(mainButton, 2, 2, "Set\nVR\nSpeed", delegate ()
 			{
 				CheetosHelpers.PopupCall("Set VR Speed", "Done", "Enter Speed. . .", true, delegate (string text)
 				{
+					float value = 0f;
 
+					try
+					{
+						value = float.Parse(text);
+					}
+					catch
+					{
+						ModConsole.Error("Input value must be a float value!");
+					}
+					finally
+					{
+						vrSpeedSlider.SetValue(value);
+						Flight.SetVRFlySpeed(value);
+					}
 				});
 			}, "Manually input a VR speed");
 		}
