77deb0c5f84e8a63098aca782fbace090041361a
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-06-09 14:48:49 +0200

    Update OnlineEditor.cs

--- AstroClient/GameObject/Online/OnlineEditor.cs
+++ AstroClient/GameObject/Online/OnlineEditor.cs
@@ -9,70 +9,36 @@
 
 	public class OnlineEditor
     {
-        public static void TakeObjectOwnership(GameObject obj)
-        {
-            try
-            {
-                if (LocalPlayerAPI != null)
-                {
-                    if (!IsLocalPlayerOwner(obj))
-                    {
-                        Networking.SetOwner(LocalPlayerAPI, obj);
-                    }
-                }
-            }
-            catch { }
-        }
 
-        public static void RemoveOwnerShip(GameObject obj)
-        {
-            Networking.SetOwner(Get_instance_master(), obj);
-        }
 
-        private static VRCPlayerApi Get_instance_master()
-        {
-            return WorldUtils.Get_Players()
-                .ToArray()
-                .ToList()
-                .Where(x => x.GetIsMaster())
-                .Select(x2 => x2.GetVRCPlayerApi())
-                .FirstOrDefault(null);
-        }
+		public static void TakeObjectOwnership(GameObject obj)
+		{
+			Networking.SetOwner(VRC.Player.prop_Player_0.field_Private_VRCPlayerApi_0, obj);
+		}
 
-        public static bool IsLocalPlayerOwner(GameObject obj)
-        {
-            if (LocalPlayerAPI != null)
-            {
-                return LocalPlayerAPI.IsOwner(obj);
-            }
-            return false;
-        }
+		public static void RemoveOwnerShip(GameObject obj)
+		{
+			Networking.SetOwner(Get_instance_master(), obj);
+		}
 
-        public static void ReturnObjectOwner(GameObject obj)
-        {
-            ModConsole.Warning("Current Owner : " + Networking.GetOwner(obj).displayName);
-        }
+		public static bool IsLocalPlayerOwner(GameObject obj)
+		{
+			return Networking.IsOwner(VRC.Player.prop_Player_0.field_Private_VRCPlayerApi_0, obj);
+		}
 
-        private static VRCPlayerApi _LocalPlayerAPI;
+		public static void ReturnObjectOwner(GameObject obj)
+		{
+			ModConsole.Warning("Current Owner : " + Networking.GetOwner(obj).displayName);
+		}
 
-        public static VRCPlayerApi LocalPlayerAPI
-        {
-            get
-            {
-                if (_LocalPlayerAPI == null)
-                {
-                    var api = Player.prop_Player_0.field_Private_VRCPlayerApi_0;
-                    if (api != null)
-                    {
-                        return _LocalPlayerAPI = api;
-                    }
-                }
-                else
-                {
-                    return _LocalPlayerAPI;
-                }
-                return null;
-            }
-        }
+		private static VRCPlayerApi Get_instance_master()
+		{
+			return WorldUtils.Get_Players()
+				.ToArray()
+				.ToList()
+				.Where(x => x.GetIsMaster())
+				.Select(x2 => x2.GetVRCPlayerApi())
+				.FirstOrDefault(null);
+		}
     }
 }
