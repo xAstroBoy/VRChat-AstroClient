e127055d0411ce8b1c4bfdd1f18535b8dd079e78
Craig Craig <craig@craig.se>
2021-06-20 13:05:47 -0500

    CMLabel color

--- AstroClient/CheetosUI/CMLabel.cs
+++ AstroClient/CheetosUI/CMLabel.cs
@@ -9,16 +9,28 @@
 
 	public class CMLabel : CMBase
 	{
-        public CMLabel(Transform parent, Vector2 position, string text) : base(parent, position)
+        public CMLabel(Transform parent, Vector2 position, string text, Color? color = null, int minTextSize = 12, int maxTextSize = 70) : base(parent, position)
         {
+			Color _color;
+
+			if (color == null)
+			{
+				_color = Color.black;
+			}
+			else
+			{
+				_color = (Color)color;
+			}
+
             GetGameObject.name = "CMLabel";
             GetGameObject.AddComponent<Text>();
             GetGameObject.GetComponent<Text>().text = text;
             GetGameObject.GetComponent<Text>().font = Font.GetDefault();
             GetGameObject.GetComponent<Text>().resizeTextForBestFit = true;
             GetGameObject.GetComponent<Text>().supportRichText = true;
-            GetGameObject.GetComponent<Text>().resizeTextMaxSize = 70;
-            GetGameObject.GetComponent<Text>().color = Color.blue;
+            GetGameObject.GetComponent<Text>().resizeTextMinSize = minTextSize;
+            GetGameObject.GetComponent<Text>().resizeTextMaxSize = maxTextSize;
+            GetGameObject.GetComponent<Text>().color = _color;
             GetGameObject.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
         }
     }
