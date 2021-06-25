8a28dd23ada83d9f7f01cc6b537ac50d403075ab
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-06-25 16:48:21 +0200

    Update SyncPhysics_ext.cs

--- AstroClient/Extensions/SyncPhysics_ext.cs
+++ AstroClient/Extensions/SyncPhysics_ext.cs
@@ -5,23 +5,23 @@
 
 	public static class SyncPhysics_ext
     {
-        public static void RefreshProperties(this SyncPhysics instance)
-        {
-            if (instance != null)
-            {
-				instance.Method_Public_Void_PDM_1();
+		public static void RefreshProperties(this SyncPhysics instance)
+		{
+			if (instance != null)
+			{
+				instance.Method_Public_Void_1();
 			}
 		}
 
-        public static void RespawnItem(this SyncPhysics instance)
-        {
-            if (instance != null)
-            {
-				instance.Method_Public_Void_PDM_2();
-            }
-        }
+		public static void RespawnItem(this SyncPhysics instance)
+		{
+			if (instance != null)
+			{
+				instance.Method_Public_Void_PDM_3();
+			}
+		}
 
-        public static Rigidbody GetRigidBody(this SyncPhysics instance)
+		public static Rigidbody GetRigidBody(this SyncPhysics instance)
         {
             return instance?.field_Private_Rigidbody_0;
         }
