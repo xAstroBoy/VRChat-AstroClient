a3ec8b055d83705c53f74b4d1e5756163832214d
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-07-12 03:18:16 +0200

    Update UdonUnboxer.cs

--- AstroClient/Udon/UdonUnboxer.cs
+++ AstroClient/Udon/UdonUnboxer.cs
@@ -2,23 +2,19 @@
 {
 	using AstroLibrary.Console;
 	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
-	using System.Threading.Tasks;
 	using UnhollowerBaseLib;
 	using VRC.Udon;
 	using VRC.Udon.Common.Interfaces;
 
 	public static class UdonUnboxer
 	{
-
 		public static void UnboxUdon(UdonBehaviour udonnode)
 		{
-			if(udonnode != null)
+			if (udonnode != null)
 			{
 				IUdonProgram program = null;
 				IUdonSymbolTable symbol_table = null;
+				IUdonHeap heap = null;
 				if (udonnode._program != null)
 				{
 					program = udonnode._program;
@@ -26,6 +22,10 @@
 				if (program != null)
 				{
 					symbol_table = program.SymbolTable;
+					if(program.Heap != null)
+					{
+						heap = program.Heap;
+					}
 				}
 				else
 				{
@@ -34,66 +34,86 @@
 				}
 
 				ModConsole.Log($"[Udon Unboxer] : Dumping {udonnode.name} Symbols and types..", System.Drawing.Color.Orange);
-				foreach(var symbol in symbol_table.GetSymbols())
+				foreach (var symbol in symbol_table.GetSymbols())
 				{
-					if(symbol != null)
+					if (symbol != null)
 					{
 						var symboltype = symbol_table.GetSymbolType(symbol);
-						ModConsole.Log($"[Udon Unboxer] : Found Symbol : {symbol}, Type : {symboltype.FullName} with value : {UnboxTypeToString(symboltype)}", System.Drawing.Color.Gold);
+						var address = symbol_table.GetAddressFromSymbol(symbol);
+
+						try
+						{
+							string unpackedsymbol = UnboxUdonHeap(heap.GetHeapVariable(address), symboltype);
+
+							ModConsole.Log($"[Udon Unboxer] : Found Symbol : {symbol}, Type : {symboltype.FullName} with value : {unpackedsymbol}", System.Drawing.Color.Gold);
+						}
+						catch (Exception e)
+						{
+							ModConsole.DebugErrorExc(e);
+							ModConsole.Log($"[Udon Unboxer] : Found Symbol : {symbol}, Type : {symboltype.FullName} (Error unpacking it.)", System.Drawing.Color.Gold);
+						}
 					}
 				}
-
-
-
-
 			}
-			
-
 		}
 
-
-
-		private static string UnboxTypeToString(Il2CppSystem.Type il2cpptype)
+		private static string UnboxUdonHeap(Il2CppSystem.Object unboxobj, Il2CppSystem.Type il2cpptype)
 		{
-			if(il2cpptype != null)
+			if (il2cpptype != null)
 			{
-				if(il2cpptype.FullName.Equals("System.String"))
+				if (il2cpptype.FullName == "System.String")
 				{
-					return IL2CPP.Il2CppStringToManaged(il2cpptype.Pointer); // TODO: Figure it out.
-				}
+					ModConsole.DebugLog("Unpacking it as : System.String");
 
+					return unboxobj.ToString();
+				}
+				else if (il2cpptype.FullName == "System.UInt32")
+				{
+					ModConsole.DebugLog("Unpacking it as : System.Int32");
 
+					return unboxobj.Unbox<System.UInt32>().ToString();
+				}
+				else if (il2cpptype.FullName == "System.Int32")
+				{
+					ModConsole.DebugLog("Unpacking it as : System.Int32");
 
+					return unboxobj.Unbox<System.Int32>().ToString();
+				}
+				else if (il2cpptype.FullName == "System.Int64")
+				{
+					ModConsole.DebugLog("Unpacking it as : System.Int64");
 
-				else if(il2cpptype.FullName.Equals("System.UInt32"))
-				{
-					return il2cpptype.Unbox<System.UInt32>().ToString();
+					return unboxobj.Unbox<System.Int64>().ToString();
 				}
-				else if (il2cpptype.FullName.Equals("System.Int32"))
+				else if (il2cpptype.FullName == "System.Char")
 				{
-					return il2cpptype.Unbox<System.Int32>().ToString();
+					ModConsole.DebugLog("Unpacking it as : System.Char");
+
+					return unboxobj.Unbox<System.Char>().ToString();
 				}
-				else if (il2cpptype.FullName.Equals("System.Int64"))
+				else if (il2cpptype.FullName == "System.Boolean")
 				{
-					return il2cpptype.Unbox<System.Int64>().ToString();
+					ModConsole.DebugLog("Unpacking it as : System.Boolean");
+
+					return unboxobj.Unbox<System.Boolean>().ToString();
 				}
-				else if (il2cpptype.FullName.Equals("System.Char"))
+				else if (il2cpptype.FullName == "System.String[]")
 				{
-					return il2cpptype.Unbox<System.Char>().ToString();
+					ModConsole.DebugLog("Unpacking it as : System.String[]");
+
+					string translated = "";
+					var il2cpplist = new Il2CppStringArray(unboxobj.Pointer);
+					foreach (var item in il2cpplist)
+					{
+						translated += item + " ";
+					}
+					return translated;
 				}
-				else if (il2cpptype.FullName.Equals("System.Boolean"))
+				else if (il2cpptype.FullName == "UnityEngine.Color")
 				{
-					return il2cpptype.Unbox<System.Boolean>().ToString();
+					ModConsole.DebugLog("Unpacking it as : UnityEngine.Color");
+					return unboxobj.Unbox<UnityEngine.Color>().ToString();
 				}
-				else if (il2cpptype.FullName.Equals("System.Boolean"))
-				{
-					return il2cpptype.Unbox<System.Boolean>().ToString();
-				}
-				else if (il2cpptype.FullName.Equals("UnityEngine.Color"))
-				{
-					return il2cpptype.Unbox<UnityEngine.Color>().ToString();
-				}
-				
 			}
 			return "Not Supported Yet";
 		}
