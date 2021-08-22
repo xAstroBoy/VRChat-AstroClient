6dcffc820bcfd51c626d9d539dfbad7e6dae163b
Craig Craig <craig@craig.se>
2021-08-22 02:53:49 -0500

    Mod detection stuff

--- AstroClient/ModDetector/FindMods.cs
+++ AstroClient/ModDetector/FindMods.cs
@@ -9,18 +9,23 @@
 
 		public override void OnApplicationStart()
 		{
-			if (MelonHandler.Mods.Any(m => m.Info.Name == "FavCat Unlocked"))
+			if (MelonHandler.Mods.Any(m => m.Info.Name == "Notorious"))
 			{
-				ModConsole.Log("Creating with FavCat Unlocked Compatibility");
-				Favcat_Unchained_Present = true;
+				ModConsole.Log("Notorious Detected! Compatibility Initialized.");
+				IsNotoriousPresent = true;
 			}
-			else
-			{
-				ModConsole.Warning("You are missing Favcat Unlocked patched by xAstroBoy, For a World pedestral Dumper, Download it from here : https://github.com/xAstroBoy/FavCat-Unchained");
-			}
+			//	if (MelonHandler.Mods.Any(m => m.Info.Name == "FavCat Unlocked"))
+			//	{
+			//		ModConsole.Log("Creating with FavCat Unlocked Compatibility");
+			//		Favcat_Unchained_Present = true;
+			//	}
+			//	else
+			//	{
+			//		ModConsole.Warning("You are missing Favcat Unlocked patched by xAstroBoy, For a World pedestral Dumper, Download it from here : https://github.com/xAstroBoy/FavCat-Unchained");
+			//	}
 		}
 
-		public static bool Favcat_Unchained_Present { get; private set; }
+		public static bool IsNotoriousPresent { get; private set; }
 
 	}
 }
