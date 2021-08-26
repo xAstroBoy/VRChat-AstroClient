91cebb8fe0be1e7c4994989bcb10788a54bc82c9
Craig Craig <craig@craig.se>
2021-08-26 12:42:12 -0500

    Debug game/client using F5

--- AstroClient/AntiCrash/AntiCrash.cs
+++ AstroClient/AntiCrash/AntiCrash.cs
@@ -1,32 +1,115 @@
-﻿namespace AstroClient.AntiCrash
+﻿namespace AstroClient.Cheetos
 {
-	using AstroClient.Variables;
+	using AstroLibrary.Console;
+	using Harmony;
+	using MelonLoader;
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Reflection;
 	using UnityEngine;
-	using VRC.SDKBase;
+	using VRC.Core;
 
-	/// <summary>
-	/// Just calling it AntiCrash, as the name is pretty generic
-	/// </summary>
 	public class AntiCrash : GameEvents
     {
-        public static bool Enabled;
+		public class Patch
+		{
+			private static List<Patch> Patches = new List<Patch>();
+			public MethodInfo TargetMethod { get; set; }
+			public HarmonyMethod PrefixMethod { get; set; }
+			public HarmonyMethod PostfixMethod { get; set; }
+			public HarmonyLib.Harmony Instance { get; set; }
 
-        public override void OnApplicationStart()
-        {
-            // Just while it's being developed
-            if (Bools.IsDebugMode)
-            {
-                Enabled = true;
-            }
-        }
+			public Patch(MethodInfo targetMethod, HarmonyMethod Before = null, HarmonyMethod After = null)
+			{
+				if (targetMethod == null || (Before == null && After == null))
+				{
+					ModConsole.Error("[Patches] TargetMethod is NULL or Pre And PostFix are Null");
+					return;
+				}
+				Instance = new HarmonyLib.Harmony($"Patch:{targetMethod.DeclaringType.FullName}.{targetMethod.Name}");
+				TargetMethod = targetMethod;
+				PrefixMethod = Before;
+				PostfixMethod = After;
+				Patches.Add(this);
+			}
 
-        public override void OnAvatarSpawn(VRCAvatarManager VRCAvatarManager, GameObject Avatar)
-        {
-            if (Enabled)
-            {
-                // Check and scan :)
-                AntiCrashScanner.ScanAvatar(Avatar, VRCAvatarManager.field_Private_VRC_AvatarDescriptor_0);
-            }
-        }
-    }
+			public static async void DoPatches()
+			{
+				foreach (var patch in Patches)
+				{
+					try
+					{
+						ModConsole.DebugLog($"[Patches] Patching {patch.TargetMethod.DeclaringType.FullName}.{patch.TargetMethod.Name} | with AstroClient {patch.PrefixMethod?.method.Name}{patch.PostfixMethod?.method.Name}");
+						patch.Instance.Patch(patch.TargetMethod, patch.PrefixMethod, patch.PostfixMethod);
+					}
+					catch (Exception e)
+					{
+						ModConsole.Error($"[Patches] Failed At {patch.TargetMethod?.Name} | {patch.PrefixMethod?.method.Name} | with AstroClient {patch.PostfixMethod?.method.Name}");
+						ModConsole.Error(e.Message);
+						ModConsole.ErrorExc(e);
+					}
+				}
+				ModConsole.DebugLog($"[Patches] Done! Patched {Patches.Count} Methods!");
+			}
+
+			public static async void UnDoPatches()
+			{
+				foreach (var patch in Patches)
+				{
+					try
+					{
+						patch.Instance.UnpatchAll();
+					}
+					catch (Exception e)
+					{
+						ModConsole.Error($"[Patches] Failed At {patch.TargetMethod?.Name} | {patch.PrefixMethod?.method.Name} | {patch.PostfixMethod?.method.Name}");
+						ModConsole.ErrorExc(e);
+					}
+				}
+				ModConsole.DebugLog($"[Patches] Done! UnPatched {Patches.Count} Methods!");
+			}
+		}
+
+		private static HarmonyMethod GetPatch(string name)
+		{
+			return new HarmonyMethod(typeof(AntiCrash).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic));
+		}
+
+		public override void ExecutePriorityPatches()
+		{
+			MelonCoroutines.Start(Init());
+		}
+
+		public static async void InitPatch()
+		{
+			try
+			{
+				ModConsole.Log("[AstroClient AntiCrash Patches] Start. . .");
+
+				//new Patch(typeof(AssetManagement).GetMethod(nameof(AssetManagement.Method_Public_Static_Object_Object_Vector3_Quaternion_Boolean_Boolean_Boolean_0)), null, GetPatch(nameof(OnAvatarPostCheck)));
+				//new Patch(typeof(AssetManagement).GetMethod(nameof(AssetManagement.Method_Public_Static_Object_Object_Vector3_Quaternion_Boolean_Boolean_Boolean_0)), GetPatch(nameof(OnAvatarPreCheck)), null);
+
+				ModConsole.DebugLog("[Client AntiCrash Patches] DONE!");
+				Patch.DoPatches();
+			}
+			catch (System.Exception e) { ModConsole.Error("Error in applying patches : " + e); }
+			finally { }
+		}
+
+		private IEnumerator Init()
+		{
+			InitPatch();
+			yield break;
+		}
+
+		private static void OnAvatarPostCheck(ref UnityEngine.Object __0, ref Vector3 __1, ref Quaternion __2, ref bool __3, ref bool __4, ref bool __5, ref UnityEngine.Object __result)
+		{
+		}
+
+		private static bool OnAvatarPreCheck(UnityEngine.Object __0, Vector3 __1, Quaternion __2, bool __3, bool __4, bool __5)
+		{
+			return true;
+		}
+	}
 }
