e409ec2173715a78e9e8c041b95f734bd4433cb1
Craig Craig <craig@craig.se>
2021-08-26 00:31:49 -0500

    Disable CheetosMenu

--- AstroClient/CheetosUI/CheetoMenu.cs
+++ AstroClient/CheetosUI/CheetoMenu.cs
@@ -20,40 +20,40 @@
 
 		public override void VRChat_OnUiManagerInit()
 		{
-			try
-			{
-				var camera = GameObject.Find("Camera (eye)").GetComponent<Camera>();
-				UI = new GameObject() { name = "CheetoUI" };
-				UI.name = "CheetoUI";
-				UI.layer = LayerMask.NameToLayer("UI");
+			//try
+			//{
+			//	var camera = GameObject.Find("Camera (eye)").GetComponent<Camera>();
+			//	UI = new GameObject() { name = "CheetoUI" };
+			//	UI.name = "CheetoUI";
+			//	UI.layer = LayerMask.NameToLayer("UI");
 
-				Menu = new GameObject() { name = "CheetoMenu" };
-				Menu.layer = LayerMask.NameToLayer("UiMenu");
-				Menu.AddComponent<RectTransform>();
-				Menu.transform.SetParent(UI.transform, false);
-				Menu.GetComponent<RectTransform>().sizeDelta = new Vector2(800, 600);
-				Menu.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.5f, 0.5f);
-				Menu.AddComponent<BoxCollider>();
-				Menu.GetComponent<BoxCollider>().enabled = false;
-				Menu.AddComponent<Canvas>();
-				Menu.GetComponent<Canvas>().renderMode = RenderMode.WorldSpace;
-				Menu.GetComponent<Canvas>().worldCamera = camera;
-				Menu.AddComponent<CanvasGroup>();
-				Menu.AddComponent<GraphicRaycaster>();
-				Menu.GetComponent<GraphicRaycaster>().enabled = true;
-				Menu.GetComponent<GraphicRaycaster>().m_BlockingMask = 0;
+			//	Menu = new GameObject() { name = "CheetoMenu" };
+			//	Menu.layer = LayerMask.NameToLayer("UiMenu");
+			//	Menu.AddComponent<RectTransform>();
+			//	Menu.transform.SetParent(UI.transform, false);
+			//	Menu.GetComponent<RectTransform>().sizeDelta = new Vector2(800, 600);
+			//	Menu.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.5f, 0.5f);
+			//	Menu.AddComponent<BoxCollider>();
+			//	Menu.GetComponent<BoxCollider>().enabled = false;
+			//	Menu.AddComponent<Canvas>();
+			//	Menu.GetComponent<Canvas>().renderMode = RenderMode.WorldSpace;
+			//	Menu.GetComponent<Canvas>().worldCamera = camera;
+			//	Menu.AddComponent<CanvasGroup>();
+			//	Menu.AddComponent<GraphicRaycaster>();
+			//	Menu.GetComponent<GraphicRaycaster>().enabled = true;
+			//	Menu.GetComponent<GraphicRaycaster>().m_BlockingMask = 0;
 
-				_ = new CMBackground(Menu.transform, Color.gray);
-				_ = new CMPage(Menu.transform);
+			//	_ = new CMBackground(Menu.transform, Color.gray);
+			//	_ = new CMPage(Menu.transform);
 
-				Menu.SetActive(false);
-				Object.DontDestroyOnLoad(UI);
-			}
-			catch (System.Exception e)
-			{
-				ModConsole.DebugError("Exception Generating CheetoMenu!");
-				ModConsole.DebugErrorExc(e);
-			}
+			//	Menu.SetActive(false);
+			//	Object.DontDestroyOnLoad(UI);
+			//}
+			//catch (System.Exception e)
+			//{
+			//	ModConsole.DebugError("Exception Generating CheetoMenu!");
+			//	ModConsole.DebugErrorExc(e);
+			//}
 		}
 
 		public override void OnUpdate()
