1e751c0b7fa74322344ca11e486ca54d3da0a3c5
Craig Craig <craig@craig.se>
2021-08-26 08:16:43 -0500

    Meroom updated

--- AstroClient/WorldAddons/Supported_Worlds/Meroom.cs
+++ AstroClient/WorldAddons/Supported_Worlds/Meroom.cs
@@ -8,41 +8,41 @@
 
 	public class Meroom : GameEvents
     {
-        public override void OnWorldReveal(string id, string Name, List<string> tags, string AssetURL)
+		public override void OnWorldReveal(string id, string Name, List<string> tags, string AssetURL)
         {
             if (id == WorldIds.Meroom)
             {
                 ModConsole.Log($"Recognized {Name} World, Spawning Lockpick Trigger.");
 
-                var PrivateRoomSwitchTrigger = GameObjectFinder.Find("Private room/Button/switch");
-                var DisplaySwitchTrigger = GameObjectFinder.Find("Living room/Button/switch (1)");
-                if (PrivateRoomSwitchTrigger == null)
-                {
-                    ModConsole.Log("Failed to Find Private Room Switch, Has MeRoom World updated?", Color.Red);
-                }
-                else
-                {
-                    ModConsole.Log("Found Private room Switch!", Color.Green);
-                }
-                if (DisplaySwitchTrigger == null)
-                {
-                    ModConsole.Log("Failed to Find Living Room Display Switch, Has MeRoom World updated?", Color.Red);
-                }
-                else
-                {
-                    ModConsole.Log("Found Living Room Display Switch!", Color.Green);
-                }
-                if (PrivateRoomSwitchTrigger == null || DisplaySwitchTrigger == null)
-                {
-                    ModConsole.Log("Aborted LockPick Button Generation.", Color.Yellow);
-                    return;
-                }
+                var PrivateRoomSwitchTrigger = GameObjectFinder.Find("Private room/Switch/Lock switch");
+                var DisplaySwitchTrigger = GameObjectFinder.Find("Living/Switch/switch");
+				if (PrivateRoomSwitchTrigger == null)
+				{
+					ModConsole.Log("Failed to Find Private Room Switch, Has MeRoom World updated?", Color.Red);
+				}
+				else
+				{
+					ModConsole.Log("Found Private room Switch!", Color.Green);
+				}
+				if (DisplaySwitchTrigger == null)
+				{
+					ModConsole.Log("Failed to Find Living Room Display Switch, Has MeRoom World updated?", Color.Red);
+				}
+				else
+				{
+					ModConsole.Log("Found Living Room Display Switch!", Color.Green);
+				}
+				if (PrivateRoomSwitchTrigger == null || DisplaySwitchTrigger == null)
+				{
+					ModConsole.Log("Aborted LockPick Button Generation.", Color.Yellow);
+					return;
+				}
 
-                if (PrivateRoomSwitchTrigger != null && DisplaySwitchTrigger != null)
-                {
-                    TriggersCloner.CloneVRC2SDKTrigger(PrivateRoomSwitchTrigger, DisplaySwitchTrigger, "Lockpick Door");
-                }
-            }
+				if (PrivateRoomSwitchTrigger != null && DisplaySwitchTrigger != null)
+				{
+					TriggersCloner.CloneVRC2SDKTrigger(PrivateRoomSwitchTrigger, DisplaySwitchTrigger, "Lockpick Door");
+				}
+			}
         }
     }
 }
