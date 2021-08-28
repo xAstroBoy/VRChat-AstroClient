b61fabf048254c5da0569ad3eabdc5cde1ad45a3
Craig Craig <craig@craig.se>
2021-08-28 15:07:16 -0500

    NamePlate Improvements

--- AstroClient/Components/NamePlates/NamePlates.cs
+++ AstroClient/Components/NamePlates/NamePlates.cs
@@ -1,11 +1,19 @@
 ﻿namespace AstroClient.Cheetos
 {
+    using AstroLibrary.Utility;
     using System;
+    using System.Collections.Generic;
+    using TMPro;
     using UnityEngine;
+    using UnityEngine.UI;
     using VRC;
 
     public class NamePlates : GameEventsBehaviour
     {
+        private PlayerNameplate nameplate;
+
+        private Player player;
+
         public NamePlates(IntPtr obj0) : base(obj0)
         {
         }
@@ -14,7 +22,29 @@
 
         private void Refresh()
         {
-            //ModConsole.Log($"Updating {GetComponent<Player>().DisplayName()}'s nameplate");
+            var text = $"";
+            if (player.IsInstanceMaster())
+            {
+                text += $"(Owner) ";
+            }
+            if (player.IsInVR())
+            {
+                text += $"[VR] ";
+            }
+            if (player.IsFriend())
+            {
+                text += $"[F] ";
+            }
+            text += $"F:{player.GetFramesColored()}|P:{player.GetPingColored()}";
+            nameplate.SetBackgroundColor(player.GetAPIUser().GetRankColor());
+            nameplate.GetTrustText().text = text;
+            nameplate.GetTrustText().color = Color.white;
+            nameplate.GetQuickStats().SetActive(true);
+            nameplate.GetSubText().SetActive(true);
+            nameplate.GetFriendIcon().gameObject.SetActive(player.IsFriend());
+            nameplate.GetPerformanceText().gameObject.SetActive(false);
+            nameplate.GetPerformanceIcon().gameObject.SetActive(false);
+            nameplate.GetTrustIcon().gameObject.SetActive(false);
         }
 
         public void Update()
@@ -32,76 +62,66 @@
 
         public void Start()
         {
-            var player = GetComponent<Player>();
+            player = GetComponent<Player>();
             if (player != null)
             {
+                nameplate = player.GetVRCPlayer().field_Public_PlayerNameplate_0;
             }
+        }
+    }
 
-            ////new BlazeTag(player, player.GetAPIUser().GetRank(), color);
+    public static class NamePlateExtensions
+    {
+        public static GameObject GetContents(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_GameObject_0;
+        }
 
-            //var nameplate = player.transform.Find("Player Nameplate/Canvas/Nameplate/");
-            //if (nameplate == null)
-            //{
-            //	ModConsole.Error($"Could not find Nameplate for {player.GetDisplayName()}");
-            //}
+        public static GameObject GetSubText(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_GameObject_1;
+        }
 
-            //var contents = nameplate.transform.Find("Contents/");
-            //if (contents == null)
-            //{
-            //	ModConsole.Error($"Could not find Contents for {player.GetDisplayName()}");
-            //}
+        public static GameObject GetQuickStats(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_GameObject_5;
+        }
 
-            //var main = contents.transform.Find("Main/");
-            //if (main == null)
-            //{
-            //	ModConsole.Error($"Could not find Main for {player.GetDisplayName()}");
-            //}
+        public static GameObject GetQuest(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_GameObject_6;
+        }
 
-            //var background = main.transform.Find("Background");
-            //if (background == null)
-            //{
-            //	ModConsole.Error($"Could not find Background for {player.GetDisplayName()}");
-            //}
+        public static GameObject GetFriendIcon(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_GameObject_15;
+        }
 
-            //var quickStats = contents.transform.Find("Quick Stats");
-            //if (quickStats == null)
-            //{
-            //	ModConsole.Error($"Could not find Quick Stats for {player.GetDisplayName()}");
-            //}
+        public static void SetBackgroundColor(this PlayerNameplate nameplate, Color color)
+        {
+            nameplate.field_Public_Graphic_1.color = color;
+            nameplate.field_Public_Graphic_2.color = color;
+            nameplate.field_Public_Graphic_3.color = Color.black;
+        }
 
-            //var trustIcon = quickStats.transform.Find("Trust Icon");
-            //if (trustIcon == null)
-            //{
-            //	ModConsole.Error($"Could not find Trust Icon for {player.GetDisplayName()}");
-            //}
-            //trustIcon.gameObject.SetActive(false);
+        public static Image GetTrustIcon(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_Image_0;
+        }
 
-            //var performanceIcon = quickStats.transform.Find("Performance Icon");
-            //if (performanceIcon == null)
-            //{
-            //	ModConsole.Error($"Could not find Performance Icon for {player.GetDisplayName()}");
-            //}
-            //performanceIcon.gameObject.SetActive(false);
+        public static Image GetPerformanceIcon(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_Image_1;
+        }
 
-            //var performanceText = quickStats.transform.Find("Performance Text");
-            //if (performanceText == null)
-            //{
-            //	ModConsole.Error($"Could not find Performance Text for {player.GetDisplayName()}");
-            //}
-            //performanceText.gameObject.SetActive(false);
+        public static TextMeshProUGUI GetPerformanceText(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_TextMeshProUGUI_4;
+        }
 
-            //var friendAnchorStats = quickStats.transform.Find("Friend Anchor Stats");
-            //if (friendAnchorStats == null)
-            //{
-            //	ModConsole.Error($"Could not find Friend Anchor Stats for {player.GetDisplayName()}");
-            //}
-            //friendAnchorStats.gameObject.SetActive(false);
-
-            //var image = background.gameObject.GetComponent<ImageThreeSlice>();
-            //if (image != null)
-            //{
-            //	image.color = color;
-            //}
+        public static TextMeshProUGUI GetTrustText(this PlayerNameplate nameplate)
+        {
+            return nameplate.field_Public_TextMeshProUGUI_3;
         }
     }
 }
