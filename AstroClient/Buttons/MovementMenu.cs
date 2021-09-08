007bac89d8b5be3b1e1472efda27f8d2b1af523d
Craig Craig <craig@craig.se>
2021-09-08 18:34:45 -0500

    Fix

--- AstroClient/Buttons/MovementMenu.cs
+++ AstroClient/Buttons/MovementMenu.cs
@@ -23,12 +23,11 @@
             JumpModifier.RocketJumpToggle.SetToggleState(ConfigManager.Movement.RocketJump);
 
             JumpModifier.JumpOverrideToggle = new QMToggleButton(temp, 2, 0, "Jump Override ON", new Action(() => { JumpModifier.IsJumpOverriden = true; }), "Jump Override OFF", new Action(() => { JumpModifier.IsJumpOverriden = false; }), "Allows you to Bypass jump Block in certain worlds.", null, null, null, false);
-            MovementSerializer.SerializerBtn = new QMToggleButton(temp, 3, 0, "Serializer ON", new Action(() => { MovementSerializer.Enabled = true; }), "Serializer OFF", new Action(() => { MovementSerializer.Enabled = false; }), "Blocks Movement packets (allows you to be invisible to others)", null, null, null, false);
-            QMFreeze.FreezePlayerOnQMOpenToggle = new QMToggleButton(temp, 4, 0, "Freeze On QM open \n ON", () => { QMFreeze.FreezePlayerOnQMOpen = true; }, "Freeze On QM Open \n OFF", () => { QMFreeze.FreezePlayerOnQMOpen = false; }, "Freeze Player On QuickMenu Open event.", null, null, null, false);
+            QMFreeze.FreezePlayerOnQMOpenToggle = new QMToggleButton(temp, 3, 0, "Freeze On QM open \n ON", () => { QMFreeze.FreezePlayerOnQMOpen = true; }, "Freeze On QM Open \n OFF", () => { QMFreeze.FreezePlayerOnQMOpen = false; }, "Freeze Player On QuickMenu Open event.", null, null, null, false);
             QMFreeze.FreezePlayerOnQMOpenToggle.SetToggleState(ConfigManager.Movement.QMFreeze);
 
-            _ = new QMSingleButton(temp, 1, 1, "Spawn Avatar Clone", new Action(() => { Clones.SpawnClone(); }), "Spawns current avi clone", null, null, true);
-            _ = new QMSingleButton(temp, 1, 1.5f, "Remove Avatar Clones", new Action(() => { Clones.RemoveClones(); }), "Removed Spawned avatars clones", null, null, true);
+            new QMSingleButton(temp, 1, 1, "Spawn Avatar Clone", new Action(() => { Clones.SpawnClone(); }), "Spawns current avi clone", null, null, true);
+            new QMSingleButton(temp, 1, 1.5f, "Remove Avatar Clones", new Action(() => { Clones.RemoveClones(); }), "Removed Spawned avatars clones", null, null, true);
         }
     }
 }
