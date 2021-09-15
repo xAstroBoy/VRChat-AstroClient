fdc455210c0ad886f7d1a23944498ffb3f9fa996
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-09-15 20:41:27 +0200

    New world lol

---
+++ AstroClient/WorldAddons/Supported_Worlds/FurryTalkAndChill.cs
@@ -0,0 +1,46 @@
+﻿namespace AstroClient
+{
+    using AstroClient.Variables;
+    using AstroLibrary.Console;
+    using AstroLibrary.Finder;
+    using System.Collections.Generic;
+    using AstroLibrary.Extensions;
+    using AstroClient.Udon;
+
+    public class FurryTalkAndChill : GameEvents
+    {
+        public override void OnWorldReveal(string id, string Name, List<string> tags, string AssetURL, string AuthorName)
+        {
+            if (id == WorldIds.Furry_Talk_And_Chill)
+            {
+                if (Bools.IsDeveloper)
+                {
+                    ModConsole.Log($"Recognized {Name} World, Hacking Admin Panel Behaviour..");
+
+                    var PanelBehaviour = UdonSearch.FindUdonEvent("AdminPanel", "_MakeLocalPlayerAdmin");
+
+                    if (PanelBehaviour != null)
+                    {
+                        var disassembledbehaviour = PanelBehaviour.UdonBehaviour.DisassembleUdonBehaviour();
+                        if (disassembledbehaviour != null)
+                        {
+                            var isCurrentAdmin = UdonHeapParser.Udon_Parse_Boolean(disassembledbehaviour, AdminSymbol);
+                            if(isCurrentAdmin != null)
+                            {
+                                if(!isCurrentAdmin.Value)
+                                {
+                                    ModConsole.Log("Unlocking Admin Panel (Forcing Admin access...)");
+                                    UdonHeapEditor.PatchHeap(disassembledbehaviour, AdminSymbol, true, true);
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private string AdminSymbol { get; } = "_isAdmin";
+    }
+}
