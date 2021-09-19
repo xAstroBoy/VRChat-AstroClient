a68e6e7b19db2372b727546eee73d878885bebb4
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-09-19 16:27:22 +0200

    Fixes and enchancements.

--- AstroClient/Streamer/StreamerProtector.cs
+++ AstroClient/Streamer/StreamerProtector.cs
@@ -9,6 +9,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using VRC;
 
     public class StreamerProtector : GameEvents
@@ -19,27 +20,60 @@
 
         public static bool IsExploitsAllowed => IsAStreamerPresent();
 
-        public static List<string> StreamerIDs { get; } = new List<string>()
+
+
+        // TODO : make something to automatically register them on Application start instead of manual registration
+        private static List<string> StreamerIDs { get; }  = new List<string>() 
         {
-        "usr_c22cc758-27ce-40e6-94c9-a4e290b55de5", // StarNovaKitty
-        "usr_03d6cba2-a1d7-45b8-b46e-419ccbc18dda", // Ruqa
-        "usr_c2f3c043-f9d5-4907-b6fd-2d02bb79e863", // SoulunaMia
-        "usr_b22748cd-de1c-4586-9bcc-33c51acc453d", // LondonLad
-        "usr_2d8fdf16-6470-4322-9696-88df092bf2f8", // The Great Fish
-        "usr_bf4846e6-e208-47cd-9837-a2ba3b98c688", // Lolathon
-        "usr_da67e7e6-1398-4fb3-8fcd-c1378dfe55f3", // MimiQu
-        "usr_03a485cc-1dde-41aa-aa17-29c9a06d5310", // Thor_ChanVR
-        "usr_d7e5a8c7-9c39-4baa-80cc-137d6ef404d9", // Ratchet232
-        "usr_3ff766cf-ddee-4fed-b321-aa3bdd2f9aa7", // Pud Pud
-        "usr_f3c3cb44-9f1d-4bcd-8c1d-d361dfa160fa", // Nifty☆
-        "usr_adf03ad7-5810-4d20-bf95-6320dcbb74ea", // Jryoko
-        "usr_81fe2da1-e841-4a1e-a46d-cea447c1b413", // YouMe
-        "usr_22b75801-bf0f-4464-b002-4b9b86252196", // PATTIIIIIIII
-        "usr_7c9eb849-4f62-446f-980b-9e000a1b238e", // minikatttttt
-        "usr_d4d6ea8a-8ad7-4d67-af81-d58a26449cca", // MrDummy_NL
-        "usr_8a3eb8c4-5d4a-41cd-8489-ef23e319f711", // Naisiri Nao
-        "usr_1bd361e0-02c1-4640-907c-458c508417a9", // Domania
+            Streamers.StarNovaKitty,
+            Streamers.Ruqaa,
+            Streamers.SoulunaMia,
+            Streamers.LondonLad,
+            Streamers.The_Great_Fish,
+            Streamers.Lolathon,
+            Streamers.MimiQu,
+            Streamers.Thor_ChanVR,
+            Streamers.Ratchet232,
+            Streamers.Pud_Pud,
+            Streamers.Nifty,
+            Streamers.Jryoko,
+            Streamers.YouMe,
+            Streamers.PATTIIIIIIII,
+            Streamers.minikatttttt,
+            Streamers.MrDummy_NL,
+            Streamers.Naisiri_Nao,
+            Streamers.Domania,
+            Streamers.Cambly,
+            Streamers.AbandonedFun,
+            Streamers.CyberChimp,
         };
+
+        struct Streamers
+        {
+            public const string StarNovaKitty = "usr_c22cc758-27ce-40e6-94c9-a4e290b55de5";
+            public const string Ruqaa = "usr_03d6cba2-a1d7-45b8-b46e-419ccbc18dda";
+            public const string SoulunaMia = "usr_c2f3c043-f9d5-4907-b6fd-2d02bb79e863";
+            public const string LondonLad = "usr_b22748cd-de1c-4586-9bcc-33c51acc453d";
+            public const string The_Great_Fish = "usr_2d8fdf16-6470-4322-9696-88df092bf2f8";
+            public const string Lolathon = "usr_bf4846e6-e208-47cd-9837-a2ba3b98c688";
+            public const string MimiQu = "usr_da67e7e6-1398-4fb3-8fcd-c1378dfe55f3";
+            public const string Thor_ChanVR = "usr_03a485cc-1dde-41aa-aa17-29c9a06d5310";
+            public const string Ratchet232 = "usr_d7e5a8c7-9c39-4baa-80cc-137d6ef404d9";
+            public const string Pud_Pud = "usr_3ff766cf-ddee-4fed-b321-aa3bdd2f9aa7";
+            public const string Nifty = "usr_f3c3cb44-9f1d-4bcd-8c1d-d361dfa160fa";
+            public const string Jryoko = "usr_adf03ad7-5810-4d20-bf95-6320dcbb74ea";
+            public const string YouMe = "usr_81fe2da1-e841-4a1e-a46d-cea447c1b413";
+            public const string PATTIIIIIIII = "usr_22b75801-bf0f-4464-b002-4b9b86252196";
+            public const string minikatttttt = "usr_7c9eb849-4f62-446f-980b-9e000a1b238e";
+            public const string MrDummy_NL = "usr_d4d6ea8a-8ad7-4d67-af81-d58a26449cca";
+            public const string Naisiri_Nao = "usr_8a3eb8c4-5d4a-41cd-8489-ef23e319f711";
+            public const string Domania = "usr_1bd361e0-02c1-4640-907c-458c508417a9";
+            public const string Cambly = "usr_0cf19c97-3fd0-4976-bb58-9cf5074887cf";
+            public const string AbandonedFun = "usr_e472bbb4-9020-4c17-b6bf-157dbcbebbeb";
+            public const string CyberChimp = "usr_23da6d77-61e7-4410-bcf6-321e88088a2c";
+
+        }
+
 
         public static bool IsAStreamerPresent()
         {
@@ -56,7 +90,7 @@
                     var userid = apiuser.id;
                     if (StreamerIDs.Contains(userid))
                     {
-                        Event_OnStreamerLeft.SafetyRaise(new PlayerEventArgs(player));
+                        Event_OnStreamerJoined.SafetyRaise(new PlayerEventArgs(player));
                         if (!StreamersInInstance.Contains(player))
                         {
                             StreamersInInstance.Add(player);
@@ -65,10 +99,43 @@
                                 CheetosHelpers.SendHudNotification($"Streamer : {apiuser.displayName} Joined!");
                                 ModConsole.Warning($"Streamer : {apiuser.displayName} Joined!");
                                 player.AddSingleTag(UnityEngine.Color.yellow, UnityEngine.Color.white, "Streamer");
-                                if (userid == "usr_c22cc758-27ce-40e6-94c9-a4e290b55de5")
+
+
+                                if (userid == Streamers.StarNovaKitty)
                                 {
                                     player.AddSingleTag(new UnityEngine.Color(255, 192, 203), UnityEngine.Color.white, "A Sweet Kitty");
                                 }
+                                else if (userid == Streamers.Cambly)
+                                {
+                                    player.AddSingleTag(UnityEngine.Color.blue, UnityEngine.Color.white, "A Clumsy Birb");
+                                }
+                                else if (userid == Streamers.Lolathon)
+                                {
+                                    player.AddSingleTag(UnityEngine.Color.red, UnityEngine.Color.white, "Clown & Prankster");
+
+                                }
+                                else if (userid == Streamers.PATTIIIIIIII)
+                                {
+                                    player.AddSingleTag(UnityEngine.Color.yellow, UnityEngine.Color.white, "Dont Touch mah Toast!");
+
+                                }
+                                else if (userid == Streamers.Ratchet232)
+                                {
+                                    player.AddSingleTag(UnityEngine.Color.red, UnityEngine.Color.white, "Official Psycho And cutie!");
+
+                                }
+
+                                else if (userid == Streamers.Thor_ChanVR)
+                                {
+                                    player.AddSingleTag(UnityEngine.Color.yellow, UnityEngine.Color.white, "Official VRChat goddess!");
+
+                                }
+                                else if (userid == Streamers.Pud_Pud)
+                                {
+                                    player.AddSingleTag(UnityEngine.Color.cyan, UnityEngine.Color.white, "Sensible Squeaky panda and Cute!");
+
+                                }
+
                             }
                         }
                     }
