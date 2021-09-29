a4ca5e2a9fc219e12a5d10d9603d76ccc4a88a23
xAstroBoy <16446639+xAstroBoy@users.noreply.github.com>
2021-09-29 21:00:40 +0200

    Revert internalization on struct worldid and streamerids

--- AstroClient/Streamer/Streamers.cs
+++ AstroClient/Streamer/Streamers.cs
@@ -1,32 +1,32 @@
 ﻿namespace AstroClient.Streamer
 {
-    internal struct Streamers
+    public struct Streamers
     {
-        internal const string StarNovaKitty = "usr_c22cc758-27ce-40e6-94c9-a4e290b55de5";
-        internal const string Ruqaa = "usr_03d6cba2-a1d7-45b8-b46e-419ccbc18dda";
-        internal const string SoulunaMia = "usr_c2f3c043-f9d5-4907-b6fd-2d02bb79e863";
-        internal const string LondonLad = "usr_b22748cd-de1c-4586-9bcc-33c51acc453d";
-        internal const string The_Great_Fish = "usr_2d8fdf16-6470-4322-9696-88df092bf2f8";
-        internal const string Lolathon = "usr_bf4846e6-e208-47cd-9837-a2ba3b98c688";
-        internal const string MimiQu = "usr_da67e7e6-1398-4fb3-8fcd-c1378dfe55f3";
-        internal const string Thor_ChanVR = "usr_03a485cc-1dde-41aa-aa17-29c9a06d5310";
-        internal const string Ratchet232 = "usr_d7e5a8c7-9c39-4baa-80cc-137d6ef404d9";
-        internal const string Pud_Pud = "usr_3ff766cf-ddee-4fed-b321-aa3bdd2f9aa7";
-        internal const string Nifty = "usr_f3c3cb44-9f1d-4bcd-8c1d-d361dfa160fa";
-        internal const string Jryoko = "usr_adf03ad7-5810-4d20-bf95-6320dcbb74ea";
-        internal const string YouMe = "usr_81fe2da1-e841-4a1e-a46d-cea447c1b413";
-        internal const string PATTIIIIIIII = "usr_22b75801-bf0f-4464-b002-4b9b86252196";
-        internal const string minikatttttt = "usr_7c9eb849-4f62-446f-980b-9e000a1b238e";
-        internal const string MrDummy_NL = "usr_d4d6ea8a-8ad7-4d67-af81-d58a26449cca";
-        internal const string Naisiri_Nao = "usr_8a3eb8c4-5d4a-41cd-8489-ef23e319f711";
-        internal const string Domania = "usr_1bd361e0-02c1-4640-907c-458c508417a9";
-        internal const string Cambly = "usr_0cf19c97-3fd0-4976-bb58-9cf5074887cf";
-        internal const string AbandonedFun = "usr_e472bbb4-9020-4c17-b6bf-157dbcbebbeb";
-        internal const string CyberChimp = "usr_23da6d77-61e7-4410-bcf6-321e88088a2c";
-        internal const string ZodiacSector5 = "usr_a8755929-cd9b-411a-a25a-92255e9de831";
-        internal const string RagingNova = "usr_ff31cb89-78f2-4f84-982e-ea570d59fef7";
-        internal const string TehFuzziest = "usr_54b3614e-6f68-4da2-a983-1a941dc0bae8";
-        internal const string DarrenZeus = "usr_880a77b6-b167-40be-8e5f-d1fab64f662f";
-        internal const string PharoZar = "usr_fd0dee56-407e-487f-8ebb-abb5d87e1d44";
+        public const string StarNovaKitty = "usr_c22cc758-27ce-40e6-94c9-a4e290b55de5";
+        public const string Ruqaa = "usr_03d6cba2-a1d7-45b8-b46e-419ccbc18dda";
+        public const string SoulunaMia = "usr_c2f3c043-f9d5-4907-b6fd-2d02bb79e863";
+        public const string LondonLad = "usr_b22748cd-de1c-4586-9bcc-33c51acc453d";
+        public const string The_Great_Fish = "usr_2d8fdf16-6470-4322-9696-88df092bf2f8";
+        public const string Lolathon = "usr_bf4846e6-e208-47cd-9837-a2ba3b98c688";
+        public const string MimiQu = "usr_da67e7e6-1398-4fb3-8fcd-c1378dfe55f3";
+        public const string Thor_ChanVR = "usr_03a485cc-1dde-41aa-aa17-29c9a06d5310";
+        public const string Ratchet232 = "usr_d7e5a8c7-9c39-4baa-80cc-137d6ef404d9";
+        public const string Pud_Pud = "usr_3ff766cf-ddee-4fed-b321-aa3bdd2f9aa7";
+        public const string Nifty = "usr_f3c3cb44-9f1d-4bcd-8c1d-d361dfa160fa";
+        public const string Jryoko = "usr_adf03ad7-5810-4d20-bf95-6320dcbb74ea";
+        public const string YouMe = "usr_81fe2da1-e841-4a1e-a46d-cea447c1b413";
+        public const string PATTIIIIIIII = "usr_22b75801-bf0f-4464-b002-4b9b86252196";
+        public const string minikatttttt = "usr_7c9eb849-4f62-446f-980b-9e000a1b238e";
+        public const string MrDummy_NL = "usr_d4d6ea8a-8ad7-4d67-af81-d58a26449cca";
+        public const string Naisiri_Nao = "usr_8a3eb8c4-5d4a-41cd-8489-ef23e319f711";
+        public const string Domania = "usr_1bd361e0-02c1-4640-907c-458c508417a9";
+        public const string Cambly = "usr_0cf19c97-3fd0-4976-bb58-9cf5074887cf";
+        public const string AbandonedFun = "usr_e472bbb4-9020-4c17-b6bf-157dbcbebbeb";
+        public const string CyberChimp = "usr_23da6d77-61e7-4410-bcf6-321e88088a2c";
+        public const string ZodiacSector5 = "usr_a8755929-cd9b-411a-a25a-92255e9de831";
+        public const string RagingNova = "usr_ff31cb89-78f2-4f84-982e-ea570d59fef7";
+        public const string TehFuzziest = "usr_54b3614e-6f68-4da2-a983-1a941dc0bae8";
+        public const string DarrenZeus = "usr_880a77b6-b167-40be-8e5f-d1fab64f662f";
+        public const string PharoZar = "usr_fd0dee56-407e-487f-8ebb-abb5d87e1d44";
     }
 }
