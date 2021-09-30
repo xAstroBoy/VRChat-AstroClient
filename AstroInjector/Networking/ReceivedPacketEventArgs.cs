ec9e36f618fd6b0992fa5e70747e14c473221a3f
Craig Craig <craig@craig.se>
2021-09-30 12:49:55 -0500

    Internalizing

---
+++ AstroInjector/Networking/ReceivedPacketEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace AstroNetworkingLibrary
+{
+    using AstroNetworkingLibrary.Serializable;
+
+    internal class ReceivedPacketEventArgs
+    {
+        internal PacketData Data { get; private set; }
+
+        internal int ClientID { get; private set; }
+
+        internal ReceivedPacketEventArgs(int clientID, PacketData data)
+        {
+            Data = data;
+            ClientID = clientID;
+        }
+    }
+}
