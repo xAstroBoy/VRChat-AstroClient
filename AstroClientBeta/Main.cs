29d21ea43b0ab3d817574fd25c59392a2b954bf3
Craig Craig <craig@craig.se>
2021-10-10 00:19:40 -0500

    Beta UI paths

--- AstroClientBeta/Main.cs
+++ AstroClientBeta/Main.cs
@@ -1,12 +1,42 @@
 ﻿namespace AstroClient
 {
+    using CheetosLibrary;
     using MelonLoader;
+    using System;
+    using System.Collections;
+    using UnityEngine;
 
     public class Main : MelonMod
     {
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("AstroClient");
+
+            _ = MelonCoroutines.Start(OnUiManagerInitCoro(() => { AfterUI(); }));
         }
+
+        private void AfterUI()
+        {
+            var junk = GameObject.Find(UIPaths.Banner);
+            if (junk != null) junk.SetActive(false);
+
+            MelonLogger.Msg("UI Initialized.");
+        }
+
+        private IEnumerator OnUiManagerInitCoro(Action code)
+        {
+            while (GameObject.Find(UIPaths.QuickMenu) == null)
+                yield return new WaitForSeconds(0.001f);
+
+            code();
+        }
+    }
+}
+
+namespace CheetosLibrary
+{
+    public class Utils
+    {
+
     }
 }
