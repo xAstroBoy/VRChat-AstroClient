af9a96c5737507d8bfcc581a40c4baff91929e3c
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2021-10-14 13:57:47 +0200

    Update AstroActionMenuPatches.cs

--- AstroClient/ActionMenu/AstroActionMenuPatches.cs
+++ AstroClient/ActionMenu/AstroActionMenuPatches.cs
@@ -83,7 +83,6 @@ namespace AstroActionMenu
                 "XXXXXXXXX"
             }); // No strings found :( Unusable for now. Scanning for methods doesnt help either as there are other functions that yield similar results
 
-        private static HarmonyLib.Harmony Harmony;
 
         public static void PatchAll()
         {
