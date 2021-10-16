42550a48ac3dac9d461a040efe7c38d249bd8b6f
Craig Craig <craig@craig.se>
2021-10-16 10:35:35 -0500

    Small tweak

--- AstroClient/CheetoLibrary/CheetoButton.cs
+++ AstroClient/CheetoLibrary/CheetoButton.cs
@@ -15,7 +15,8 @@
         public CheetoButton(Transform parent, string label, Action action, bool jump = false) : base(GameObject.Find(UIPaths.WorldButton), parent)
         {
             SetName($"CL-{CheetoButtonAPI.UIElements.Count}-CheetoButton:{label}");
-            Self.transform.GetComponentInChildren<TextMeshProUGUI>().text = label;
+            var text = Self.transform.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = label; text.richText = true; text.autoSizeTextContainer = true;
             SetAction(action);
             LoadSprite(CheetoUtils.ExtractResource(Assembly.GetExecutingAssembly(), "AstroClient.Resources.planet.png"));
             if (!jump) { Self.transform.Find("Badge_MMJump").gameObject.SetActive(false); }
