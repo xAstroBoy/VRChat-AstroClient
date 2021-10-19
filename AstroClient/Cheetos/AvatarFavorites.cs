6cdd114c7a6a2f542f71551a035c8d9dee167f0b
Craig Craig <craig@craig.se>
2021-10-19 15:46:03 -0500

    Temp fix

--- AstroClient/Cheetos/AvatarFavorites.cs
+++ AstroClient/Cheetos/AvatarFavorites.cs
@@ -57,7 +57,7 @@
         {
             if (!initialized)
             {
-                RefreshList();
+                //RefreshList();
                 initialized = true;
             }
         }
