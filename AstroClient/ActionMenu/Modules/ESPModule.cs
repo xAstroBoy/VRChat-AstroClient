6c5e158bde30c52d256992b90efaf35e29006b89
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2021-10-22 17:33:46 +0200

    More actionmenu stuff

--- AstroClient/ActionMenu/Modules/ESPModule.cs
+++ AstroClient/ActionMenu/Modules/ESPModule.cs
@@ -1,37 +1,14 @@
-using MelonLoader;
-using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
-using UnityEngine.SceneManagement;
-using System;
-using System.Reflection;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Collections;
-using System.Collections.Generic;
-using VRC.Playables;
-using VRC.SDKBase;
-using VRC;
-using VRC.SDK3.Avatars.ScriptableObjects;
-
 namespace AstroClient.AvatarParametersEditor
 {
-    using System.Drawing;
-    using ActionMenu;
     using AstroActionMenu.Api;
-    using AstroLibrary;
     using AstroLibrary.Console;
-    using AstroLibrary.Extensions;
-    using CheetoLibrary;
-    using Components;
     using Startup.Buttons;
-    using UnityEngine.Playables;
-    using WorldLights;
+    using System.Drawing;
 
     internal class ESPModule : GameEvents
     {
         internal override void OnApplicationStart()
         {
-
             AMUtils.AddToModsFolder("ESP Options", () =>
             {
                 // TODO: Add Textures!
@@ -42,16 +19,10 @@ namespace AstroClient.AvatarParametersEditor
                     CustomSubMenu.AddToggle("Toggle VRC Interactable ESP", VRChat_Map_ESP_Menu.Toggle_VRCInteractable_ESP, ToggleValue => { VRChat_Map_ESP_Menu.Toggle_VRCInteractable_ESP = ToggleValue; }, null, false);
                     CustomSubMenu.AddToggle("Toggle Trigger ESP", VRChat_Map_ESP_Menu.Toggle_Trigger_ESP, ToggleValue => { VRChat_Map_ESP_Menu.Toggle_Trigger_ESP = ToggleValue; }, null, false);
                     CustomSubMenu.AddToggle("Toggle Udon Behaviour ESP", VRChat_Map_ESP_Menu.Toggle_UdonBehaviour_ESP, ToggleValue => { VRChat_Map_ESP_Menu.Toggle_UdonBehaviour_ESP = ToggleValue; }, null, false);
-
-
-
                 }, null, false, null);
             });
 
             ModConsole.Log("ESP Module is ready!", Color.Green);
         }
-
-        
-
     }
 }
--- AstroClient/ActionMenu/Modules/LightsModule.cs
+++ AstroClient/ActionMenu/Modules/LightsModule.cs
@@ -1,50 +1,27 @@
-using MelonLoader;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
-using UnityEngine.SceneManagement;
-using System;
-using System.Reflection;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Collections;
-using System.Collections.Generic;
-using VRC.Playables;
-using VRC.SDKBase;
-using VRC;
-using VRC.SDK3.Avatars.ScriptableObjects;
 
 namespace AstroClient.AvatarParametersEditor
 {
-    using System.Drawing;
-    using ActionMenu;
     using AstroActionMenu.Api;
-    using AstroLibrary;
     using AstroLibrary.Console;
-    using AstroLibrary.Extensions;
-    using CheetoLibrary;
-    using Components;
-    using UnityEngine.Playables;
+    using System.Drawing;
     using WorldLights;
 
     internal class ActionLightsMenu : GameEvents
     {
         internal override void OnApplicationStart()
         {
-
             AMUtils.AddToModsFolder("Lights Option", () =>
             {
                 // TODO: Add Textures!
-                CustomSubMenu.AddToggle("Toggle Fullbright (RenderSettings)", LightControl.FullbrightByRender, ToggleValue => { LightControl.FullbrightByRender = ToggleValue; }, null, false); 
+                CustomSubMenu.AddToggle("Toggle Fullbright (RenderSettings)", LightControl.FullbrightByRender, ToggleValue => { LightControl.FullbrightByRender = ToggleValue; }, null, false);
                 CustomSubMenu.AddToggle("Toggle RenderSettings Fog", RenderSettings.fog, ToggleValue => { LightControl.ToggleFog(ToggleValue); }, null, false);
                 CustomSubMenu.AddToggle("Toggle Fullbright (Head)", LightControl.IsHeadLightActive, ToggleValue => { LightControl.IsHeadLightActive = ToggleValue; }, null, false);
-                CustomSubMenu.AddButton("Spawn Flashlight", () => {Astro_Flashlight.SpawnFlashlight(); }, null, false);
-                CustomSubMenu.AddButton("Destroy Flashlights", () => {Astro_Flashlight.DestroyAllFlashLights(); }, null, false);
+                CustomSubMenu.AddButton("Spawn Flashlight", () => { Astro_Flashlight.SpawnFlashlight(); }, null, false);
+                CustomSubMenu.AddButton("Destroy Flashlights", () => { Astro_Flashlight.DestroyAllFlashLights(); }, null, false);
             });
 
             ModConsole.Log("Lights Module Editor is ready!", Color.Green);
         }
-
-        
-
     }
 }
