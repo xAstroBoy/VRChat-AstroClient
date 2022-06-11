b3f434e8c079e609094c6ee6d0db83f0ddfc50cf
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2022-06-11 21:20:13 +0200

    Update FixRandomQMOpen.cs

--- AstroClient/Startup/Fixes/FixRandomQMOpen.cs
+++ AstroClient/Startup/Fixes/FixRandomQMOpen.cs
@@ -1,6 +1,6 @@
-﻿using AstroClient.ClientActions;
+﻿using System.Collections.Generic;
+using AstroClient.ClientActions;
 using AstroClient.xAstroBoy.AstroButtonAPI.Tools;
-using Il2CppSystem.Collections.Generic;
 using UnityEngine;
 using VRC.UI.Elements;
 
@@ -38,12 +38,58 @@ namespace AstroClient.Startup.Hooks
 
         private static bool BlockAndReport(string __0, bool __1, bool __2)
         {
-            //Log.Debug($"MenuStateController.Method_Public_Void_String_Boolean_Boolean_2 Has Been Invoked with params :" +
-            //$" string param_0 = {__0} , bool : {__1}, bool : {__2}");
-
+            if (IsVRChatPage(__0))
+            {
+                return true;
+            }
             return false;
-
         }
 
+
+
+        private static bool IsVRChatPage(string pagename)
+        {
+            switch (pagename)
+            {
+                case "Modal_ConfirmDialog":
+                case "Modal_HoveredUser":
+                case "Modal_QM_ShowAvatarInteractionInfo":
+                case "Modal_ConfirmListDialog":
+                case "Modal_QM_ViewPhoto":
+                case "Modal_QM_MoreActions":
+                case "Modal_QM_AddAvatarToFavorites":
+                case "Modal_AddWorldToPlaylist":
+                case "Modal_AddMessage":
+                case "Menu_QM_AvatarDetails":
+                case "Menu_ChangeAudioInputDevice":
+                case "Menu_UserIconCamera":
+                case "Menu_QM_GiftVRCPlus":
+                case "Menu_QM_Report_Details":
+                case "Menu_QM_Report_Issue":
+                case "Menu_QM_Gallery":
+                case "Menu_InviteResponse":
+                case "Menu_SelectedUser_Remote":
+                case "Menu_SelectedUser_Local":
+                case "Menu_DevTools":
+                case "Menu_QM_Emojis":
+                case "Menu_AudioSettings":
+                case "Menu_Settings":
+                case "Menu_Here":
+                case "Menu_Camera":
+                case "Menu_Notifications":
+                case "Menu_Dashboard":
+                case "Wing_Menu_Worlds_Filter":
+                case "Wing_Menu_Worlds":
+
+                    return true;
+
+
+                default:
+                    return false;
+            }
+
+
+
+        }
     }
 }
