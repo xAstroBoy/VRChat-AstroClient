15a23a10abde881d81e5276b6486f6cccbb27eb3
Craig Craig <craig@craig.se>
2022-07-07 11:34:14 -0500

    Todos

--- AstroClientV2/AstroClient.cs
+++ AstroClientV2/AstroClient.cs
@@ -1,11 +1,20 @@
 ﻿namespace AstroClientV2;
 
 #region Imports
-
 using MelonLoader;
-
 #endregion
 
 public class AstroClient : MelonMod
 {
+    public override void OnPreSupportModule()
+    {
+        // TODO: Config Manager
+    }
+
+    public override void OnApplicationStart()
+    {
+        // TODO: CheetahNet Authentication
+
+        // TODO: Module System
+    }
 }
---
+++ AstroClientV2/UIManager.cs
@@ -0,0 +1,11 @@
+﻿namespace AstroClientV2;
+
+/// <summary>
+/// UI Management for the entire client
+/// </summary>
+public static class UIManager
+{
+    public static void Initialize()
+    {
+    }
+}
