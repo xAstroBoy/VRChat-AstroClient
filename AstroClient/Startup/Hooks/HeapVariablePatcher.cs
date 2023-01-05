b4f5541ccdff3208c3fbd1eb308193a009a4ced8
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2023-01-05 20:06:36 +0100

    Reee

--- AstroClient/Startup/Hooks/HeapVariablePatcher.cs
+++ AstroClient/Startup/Hooks/HeapVariablePatcher.cs
@@ -28,10 +28,31 @@ namespace AstroClient.Tools.UdonEditor
 
         internal override void ExecutePriorityPatches()
         {
-            //new AstroPatch(typeof(UdonHeap).GetMethod(nameof(UdonHeap.GetHeapVariableInternal)).MakeGenericMethod(typeof(System.Object)), null, GetPatch(nameof(PlayerStartPatch)));
+            var GetHeapVariableInternal = (from m in typeof(UdonHeap).GetMethods()
+                                    where m.Name.Equals("GetHeapVariableInternal") && m.IsGenericMethod
+                                    select m).First();
+            //foreach (var item in GetHeapVariableInternal.GetGenericArguments())
+            //{
+            //    Log.Write($"GetHeapVariable supports : {item.FullName}");
+            //}
+
+            new AstroPatch(GetHeapVariableInternal.MakeGenericMethod(new[] {typeof(System.Object)}), null, GetPatch(nameof(GetHeapVariableInternal_System_Object_Postfix)));
+            new AstroPatch(GetHeapVariableInternal.MakeGenericMethod(new[] {typeof(Il2CppSystem.Object)}), null, GetPatch(nameof(GetHeapVariableInternal_Il2CppSystem_Object_Postfix)));
+            new AstroPatch(GetHeapVariableInternal.MakeGenericMethod(new[] {typeof(UnityEngine.Object)}), null, GetPatch(nameof(GetHeapVariableInternal_UnityEngine_Object_Postfix)));
+
             //new AstroPatch(typeof(UdonHeap).GetMethod(nameof(UdonHeap)), null, GetPatch(nameof(PlayerStartPatch)));
         }
 
+        private static void GetHeapVariableInternal_System_Object_Postfix(ref VRC.Udon.Common.UdonHeap __instance, ref System.Object __result, ref uint __0)
+        {
+        }
+        private static void GetHeapVariableInternal_Il2CppSystem_Object_Postfix(ref VRC.Udon.Common.UdonHeap __instance, ref Il2CppSystem.Object __result, ref uint __0)
+        {
+        }
+
+        private static void GetHeapVariableInternal_UnityEngine_Object_Postfix(ref VRC.Udon.Common.UdonHeap __instance, ref UnityEngine.Object __result, ref uint __0)
+        {
+        }
 
 
 
--- AstroClient/Startup/welcome.cs
+++ AstroClient/Startup/welcome.cs
@@ -16,16 +16,16 @@ namespace AstroClient.Startup
             ClientEventActions.OnWorldReveal += OnWorldReveal;
         }
 
-
+        private  float WelcomeDuration { get; } = 3f;
         private void OnWorldReveal(string id, string Name, List<string> tags, string AssetURL, string AuthorName)
         {
-            HudNotifier.WriteHudMessage($"Welcome Back <color=#c242f5>{PlayerUtils.DisplayName()}</color>", 10f);
-            HudNotifier.WriteHudMessage("<color=#42f5f2>AstroClient</color> Made By", 10f);
-            HudNotifier.WriteHudMessage("<rainb>AstroBoy</rainb> and <color=#F79239>Cheetos</color>", 10f);
+            HudNotifier.WriteHudMessage($"Welcome Back <color=#c242f5>{PlayerUtils.DisplayName()}</color>", WelcomeDuration);
+            HudNotifier.WriteHudMessage("<color=#42f5f2>AstroClient</color> Made By", WelcomeDuration);
+            HudNotifier.WriteHudMessage("<rainb>AstroBoy</rainb> and <color=#F79239>Cheetos</color>", WelcomeDuration);
 
             if (Bools.IsDeveloper)
             {
-                HudNotifier.WriteHudMessage("<rainb>Developer Mode!</rainb>", 10f);
+                HudNotifier.WriteHudMessage("<rainb>Developer Mode!</rainb>", WelcomeDuration);
             }
 
             ClientEventActions.OnWorldReveal -= OnWorldReveal;
